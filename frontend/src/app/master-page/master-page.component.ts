85c4a3480bb08935e7d6e985da94151f9103b18f
Simon Lindh <simonlindh.se@gmail.com>
2019-07-27 18:43:17 +0300

    TV full screen view.

---
+++ frontend/src/app/master-page/master-page.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { MemPoolService } from '../services/mem-pool.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-master-page',
+  templateUrl: './master-page.component.html',
+  styleUrls: ['./master-page.component.scss']
+})
+export class MasterPageComponent implements OnInit {
+
+  navCollapsed = false;
+  isOffline = false;
+  searchForm: FormGroup;
+
+  constructor(
+    private memPoolService: MemPoolService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+  ) { }
+
+  ngOnInit() {
+    this.searchForm = this.formBuilder.group({
+      txId: ['', Validators.pattern('^[a-fA-F0-9]{64}$')],
+    });
+
+    this.memPoolService.isOffline$
+      .subscribe((state) => {
+        this.isOffline = state;
+      });
+  }
+
+  collapse(): void {
+    this.navCollapsed = !this.navCollapsed;
+  }
+
+  search() {
+    const txId = this.searchForm.value.txId;
+    if (txId) {
+      if (window.location.pathname === '/' || window.location.pathname.substr(0, 4) === '/tx/') {
+        window.history.pushState({}, '', `/tx/${txId}`);
+      } else {
+        this.router.navigate(['/tx/', txId]);
+      }
+      this.memPoolService.txIdSearch$.next(txId);
+      this.searchForm.setValue({
+        txId: '',
+      });
+      this.collapse();
+    }
+  }
+
+}
