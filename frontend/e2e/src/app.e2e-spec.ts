7e7b536acb1a19612303ee1cb606497948d7e68b
softsimon <softsimon@protonmail.com>
2020-04-28 00:14:29 +0700

    Renaming angular project to 'mempool' from 'mempoolspace'

--- frontend/e2e/src/app.e2e-spec.ts
+++ frontend/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to mempoolspace!');
+    expect(page.getTitleText()).toEqual('Welcome to mempool!');
   });
 
   afterEach(async () => {
