65b01ccc603691d61d8f231861d3a85868853aaa
softsimon <softsimon@protonmail.com>
2020-05-02 14:04:33 +0700

    Sync asset registry on build. refs #37

---
+++ frontend/sync-asset-registry.js
@@ -0,0 +1,21 @@
+var https = require('https');
+var fs = require('fs');
+
+var PATH = 'dist/mempool/assets/';
+if (process.argv[2] && process.argv[2] === 'dev') {
+  PATH = 'src/assets/';
+}
+
+function download(filename, url) {
+  var file = fs.createWriteStream(filename);
+  https.get(url, function(response) {
+    response.pipe(file);
+  });
+}
+
+console.log('Downloading assets');
+download(PATH + 'assets.json', 'https://raw.githubusercontent.com/Blockstream/asset_registry_db/master/index.json');
+console.log('Downloading assets minimal');
+download(PATH + 'assets.minimal.json', 'https://raw.githubusercontent.com/Blockstream/asset_registry_db/master/index.minimal.json');
+console.log('Downloading asset icons');
+download(PATH + 'asset.icons.json', 'https://raw.githubusercontent.com/Blockstream/asset_registry_db/master/icons.json');
