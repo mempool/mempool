da4ce0855c784bf3e2ea178370f79ed8f41f7577
softsimon <softsimon@protonmail.com>
2020-05-30 21:18:53 +0700

    Block transactions list now use pagination instead of infinite scroll.

--- frontend/src/app/app.constants.ts
+++ frontend/src/app/app.constants.ts
@@ -33,3 +33,5 @@ export const mempoolFeeColors = [
 
 export const feeLevels = [1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20, 30, 40, 50, 60, 70, 80, 90, 100, 125, 150, 175, 200,
   250, 300, 350, 400, 500, 600, 700, 800, 900, 1000, 1200, 1400, 1600, 1800, 2000];
+
+export const ELCTRS_ITEMS_PER_PAGE = 25;
