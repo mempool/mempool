cfb60c0badac7b4a8b5f3ec14db07d18ad3eb243
softsimon <softsimon@protonmail.com>
2020-07-19 12:57:41 +0700

    Update Bisq API paths to /api/bisq

--- frontend/src/app/bisq/bisq-api.service.ts
+++ frontend/src/app/bisq/bisq-api.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { BisqTransaction, BisqBlock, BisqStats } from './bisq.interfaces';
 
-const API_BASE_URL = '/api/v1';
+const API_BASE_URL = '/bisq/api';
 
 @Injectable({
   providedIn: 'root'
@@ -16,26 +16,26 @@ export class BisqApiService {
   ) { }
 
   getStats$(): Observable<BisqStats> {
-    return this.httpClient.get<BisqStats>(API_BASE_URL + '/bisq/stats');
+    return this.httpClient.get<BisqStats>(API_BASE_URL + '/stats');
   }
 
   getTransaction$(txId: string): Observable<BisqTransaction> {
-    return this.httpClient.get<BisqTransaction>(API_BASE_URL + '/bisq/tx/' + txId);
+    return this.httpClient.get<BisqTransaction>(API_BASE_URL + '/tx/' + txId);
   }
 
   listTransactions$(start: number, length: number): Observable<HttpResponse<BisqTransaction[]>> {
-    return this.httpClient.get<BisqTransaction[]>(API_BASE_URL + `/bisq/txs/${start}/${length}`, { observe: 'response' });
+    return this.httpClient.get<BisqTransaction[]>(API_BASE_URL + `/txs/${start}/${length}`, { observe: 'response' });
   }
 
   getBlock$(hash: string): Observable<BisqBlock> {
-    return this.httpClient.get<BisqBlock>(API_BASE_URL + '/bisq/block/' + hash);
+    return this.httpClient.get<BisqBlock>(API_BASE_URL + '/block/' + hash);
   }
 
   listBlocks$(start: number, length: number): Observable<HttpResponse<BisqBlock[]>> {
-    return this.httpClient.get<BisqBlock[]>(API_BASE_URL + `/bisq/blocks/${start}/${length}`, { observe: 'response' });
+    return this.httpClient.get<BisqBlock[]>(API_BASE_URL + `/blocks/${start}/${length}`, { observe: 'response' });
   }
 
   getAddress$(address: string): Observable<BisqTransaction[]> {
-    return this.httpClient.get<BisqTransaction[]>(API_BASE_URL + '/bisq/address/' + address);
+    return this.httpClient.get<BisqTransaction[]>(API_BASE_URL + '/address/' + address);
   }
 }
