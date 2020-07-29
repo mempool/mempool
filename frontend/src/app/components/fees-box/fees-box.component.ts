9e0097e7b69fa226e0ea94e8d1fa1cd402677805
softsimon <softsimon@protonmail.com>
2020-07-29 15:16:09 +0700

    Adding fee estimation component to front page fixes #98

---
+++ frontend/src/app/components/fees-box/fees-box.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { StateService } from 'src/app/services/state.service';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+interface FeeEstimations {
+  fastestFee: number;
+  halfHourFee: number;
+  hourFee: number;
+}
+
+@Component({
+  selector: 'app-fees-box',
+  templateUrl: './fees-box.component.html',
+  styleUrls: ['./fees-box.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FeesBoxComponent implements OnInit {
+  feeEstimations$: Observable<FeeEstimations>;
+
+  constructor(
+    private stateService: StateService,
+  ) { }
+
+  ngOnInit(): void {
+    this.feeEstimations$ = this.stateService.mempoolBlocks$
+      .pipe(
+        map((pBlocks) => {
+          let firstMedianFee = Math.ceil(pBlocks[0].medianFee);
+
+          if (pBlocks.length === 1 && pBlocks[0].blockVSize <= 500000) {
+            firstMedianFee = 1;
+          }
+
+          const secondMedianFee = pBlocks[1] ? Math.ceil(pBlocks[1].medianFee) : firstMedianFee;
+          const thirdMedianFee = pBlocks[2] ? Math.ceil(pBlocks[2].medianFee) : secondMedianFee;
+
+          return {
+            'fastestFee': firstMedianFee,
+            'halfHourFee': secondMedianFee,
+            'hourFee': thirdMedianFee,
+          };
+        })
+      );
+  }
+
+}
