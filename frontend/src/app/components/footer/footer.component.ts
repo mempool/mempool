c1caaa37aa8501ff9c508eb7dc6b96a9946186f0
softsimon <softsimon@protonmail.com>
2020-07-30 13:50:21 +0700

    Refactor footer component to using OnPush.

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,61 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { StateService } from 'src/app/services/state.service';
 import { MemPoolState } from 'src/app/interfaces/websocket.interface';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+interface MempoolBlocksData {
+  blocks: number;
+  size: number;
+}
+
+interface MempoolInfoData {
+  memPoolInfo: MemPoolState;
+  progressWidth: string;
+  progressClass: string;
+}
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent implements OnInit {
-  memPoolInfo: MemPoolState | undefined;
-  mempoolBlocks = 0;
-  progressWidth = '';
-  progressClass: string;
-  mempoolSize = 0;
+  mempoolBlocksData$: Observable<MempoolBlocksData>;
+  mempoolInfoData$: Observable<MempoolInfoData>;
 
   constructor(
     private stateService: StateService,
   ) { }
 
   ngOnInit() {
-    this.stateService.mempoolStats$
-      .subscribe((mempoolState) => {
-        this.memPoolInfo = mempoolState;
-        this.updateProgress();
-      });
+    this.mempoolInfoData$ = this.stateService.mempoolStats$
+      .pipe(
+        map((mempoolState) => {
+          const vBytesPerSecondLimit = 1667;
+          let vBytesPerSecond = mempoolState.vBytesPerSecond;
+          if (vBytesPerSecond > 1667) {
+            vBytesPerSecond = 1667;
+          }
 
-    this.stateService.mempoolBlocks$
-      .subscribe((mempoolBlocks) => {
-        if (!mempoolBlocks.length) { return; }
-        const size = mempoolBlocks.map((m) => m.blockSize).reduce((a, b) => a + b);
-        const vsize = mempoolBlocks.map((m) => m.blockVSize).reduce((a, b) => a + b);
-        this.mempoolSize = size;
-        this.mempoolBlocks = Math.ceil(vsize / 1000000);
-      });
-  }
+          const percent = Math.round((vBytesPerSecond / vBytesPerSecondLimit) * 100);
 
-  updateProgress() {
-    if (!this.memPoolInfo) {
-      return;
-    }
+          let progressClass = 'bg-danger';
+          if (percent <= 75) {
+            progressClass = 'bg-success';
+          } else if (percent <= 99) {
+            progressClass = 'bg-warning';
+          }
 
-    const vBytesPerSecondLimit = 1667;
+          return {
+            memPoolInfo: mempoolState,
+            progressWidth: percent + '%',
+            progressClass: progressClass,
+          };
+        })
+      );
 
-    let vBytesPerSecond = this.memPoolInfo.vBytesPerSecond;
-    if (vBytesPerSecond > 1667) {
-      vBytesPerSecond = 1667;
-    }
+    this.mempoolBlocksData$ = this.stateService.mempoolBlocks$
+      .pipe(
+        map((mempoolBlocks) => {
+          const size = mempoolBlocks.map((m) => m.blockSize).reduce((a, b) => a + b, 0);
+          const vsize = mempoolBlocks.map((m) => m.blockVSize).reduce((a, b) => a + b, 0);
 
-    const percent = Math.round((vBytesPerSecond / vBytesPerSecondLimit) * 100);
-    this.progressWidth = percent + '%';
-
-    if (percent <= 75) {
-      this.progressClass = 'bg-success';
-    } else if (percent <= 99) {
-      this.progressClass = 'bg-warning';
-    } else {
-      this.progressClass = 'bg-danger';
-    }
+          return {
+            size: size,
+            blocks: Math.ceil(vsize / 1000000)
+          };
+        })
+      );
   }
 }
