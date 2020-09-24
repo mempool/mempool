8dddd6e25e7987c23bf03061580a2f42134e60ed
wiz <j@wiz.biz>
2020-09-24 17:12:53 +0900

    Use mempool's fork of Blockstream's Liquid asset registry db repo

--- frontend/sync-assets.js
+++ frontend/sync-assets.js
@@ -19,8 +19,8 @@ function download(filename, url) {
 }
 
 console.log('Downloading assets');
-download(PATH + 'assets.json', 'https://raw.githubusercontent.com/Blockstream/asset_registry_db/master/index.json');
+download(PATH + 'assets.json', 'https://raw.githubusercontent.com/mempool/asset_registry_db/master/index.json');
 console.log('Downloading assets minimal');
-download(PATH + 'assets.minimal.json', 'https://raw.githubusercontent.com/Blockstream/asset_registry_db/master/index.minimal.json');
+download(PATH + 'assets.minimal.json', 'https://raw.githubusercontent.com/mempool/asset_registry_db/master/index.minimal.json');
 console.log('Downloading mining pools info');
 download(PATH + 'pools.json', 'https://raw.githubusercontent.com/btccom/Blockchain-Known-Pools/master/pools.json');
