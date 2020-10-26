69aa6aa938831b289b08c226c7efd5e95c40ebfb
softsimon <softsimon@protonmail.com>
2020-10-26 19:33:20 +0700

    Handle weird response body when fetching Bisq price. fixes #145

--- backend/src/api/bisq/bisq.ts
+++ backend/src/api/bisq/bisq.ts
@@ -139,7 +139,7 @@ class Bisq {
 
   private updatePrice() {
     request('https://markets.bisq.network/api/trades/?market=bsq_btc', { json: true }, (err, res, trades: BisqTrade[]) => {
-      if (err) { return logger.err('Error updating Bisq market price: ' + err); }
+      if (err || !Array.isArray(trades)) { return logger.err('Error updating Bisq market price: ' + err); }
 
       const prices: number[] = [];
       trades.forEach((trade) => {
