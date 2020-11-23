9bd477d54520c9a8fee05fce7391f54f94947bfe
softsimon <softsimon@protonmail.com>
2020-11-23 15:04:41 +0700

    Remove ssr prepended url from asset service.

--- frontend/src/app/services/assets.service.ts
+++ frontend/src/app/services/assets.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
-import { StateService } from './state.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,15 +13,9 @@ export class AssetsService {
 
   constructor(
     private httpClient: HttpClient,
-    private stateService: StateService,
   ) {
-    let baseApiUrl = '';
-    if (!this.stateService.isBrowser) {
-      baseApiUrl = this.stateService.env.BACKEND_ABSOLUTE_URL;
-    }
-
-    this.getAssetsJson$ = this.httpClient.get(baseApiUrl + '/resources/assets.json').pipe(shareReplay());
-    this.getAssetsMinimalJson$ = this.httpClient.get(baseApiUrl + '/resources/assets.minimal.json').pipe(shareReplay());
-    this.getMiningPools$ = this.httpClient.get(baseApiUrl + '/resources/pools.json').pipe(shareReplay());
+    this.getAssetsJson$ = this.httpClient.get('/resources/assets.json').pipe(shareReplay());
+    this.getAssetsMinimalJson$ = this.httpClient.get('/resources/assets.minimal.json').pipe(shareReplay());
+    this.getMiningPools$ = this.httpClient.get('/resources/pools.json').pipe(shareReplay());
   }
 }
