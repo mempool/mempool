2891582619d47476a0b0b9a6860a20233e03520e
wiz <j@wiz.biz>
2020-11-30 03:50:53 +0900

    Fix time-since component to include "ago" after relative time string

--- frontend/src/app/components/time-since/time-since.component.ts
+++ frontend/src/app/components/time-since/time-since.component.ts
@@ -46,7 +46,7 @@ export class TimeSinceComponent implements OnInit, OnChanges, OnDestroy {
   calculate() {
     const seconds = Math.floor((+new Date() - +new Date(this.time * 1000)) / 1000);
     if (seconds < 60) {
-      return '< 1 '+$localize`:@@time.minute:`;
+      return '< 1 '+$localize`:@@time-since.minute.ago:minute ago`;
     }
     let counter, unit;
     for (const i in this.intervals) {
@@ -55,39 +55,39 @@ export class TimeSinceComponent implements OnInit, OnChanges, OnDestroy {
         if (counter > 0) {
           if (counter === 1) {
             switch (i) { // singular (1 day ago)
-              case 'year': unit = $localize`:@@time.year:`; break;
-              case 'month': unit = $localize`:@@time.month:`; break;
-              case 'week': unit = $localize`:@@time.week:`; break;
-              case 'day': unit = $localize`:@@time.day:`; break;
-              case 'hour': unit = $localize`:@@time.hour:`; break;
+              case 'year': unit = $localize`:@@time-since.year.ago:year ago`; break;
+              case 'month': unit = $localize`:@@time-since.month.ago:month ago`; break;
+              case 'week': unit = $localize`:@@time-since.week.ago:week ago`; break;
+              case 'day': unit = $localize`:@@time-since.day.ago:day ago`; break;
+              case 'hour': unit = $localize`:@@time-since.hour.ago:hour ago`; break;
               case 'minute':
-		unit = $localize`:@@time.minute:`;
+		unit = $localize`:@@time-since.minute.ago:minute ago`;
                 if (document.body.clientWidth < 768)
-		  unit = $localize`:@@time.min:`;
+		  unit = $localize`:@@time-since.min.ago:min ago`;
 	        break;
               case 'second':
-		unit = $localize`:@@time.second:`;
+		unit = $localize`:@@time-since.second.ago:second ago`;
                 if (document.body.clientWidth < 768)
-		  unit = $localize`:@@time.sec:`;
+		  unit = $localize`:@@time-since.sec.ago:sec ago`;
 	        break;
             }
             return counter + ' ' + unit;
           } else {
             switch (i) { // plural (2 days ago)
-              case 'year': unit = $localize`:@@time.years:`; break;
-              case 'month': unit = $localize`:@@time.months:`; break;
-              case 'week': unit = $localize`:@@time.weeks:`; break;
-              case 'day': unit = $localize`:@@time.days:`; break;
-              case 'hour': unit = $localize`:@@time.hours:`; break;
+              case 'year': unit = $localize`:@@time-since.years.ago:years ago`; break;
+              case 'month': unit = $localize`:@@time-since.months.ago:months ago`; break;
+              case 'week': unit = $localize`:@@time-since.weeks.ago:weeks ago`; break;
+              case 'day': unit = $localize`:@@time-since.days.ago:days ago`; break;
+              case 'hour': unit = $localize`:@@time-since.hours.ago:hours ago`; break;
               case 'minute':
-		unit = $localize`:@@time.minutes:`;
+		unit = $localize`:@@time-since.minutes.ago:minutes ago`;
                 if (document.body.clientWidth < 768)
-		  unit = $localize`:@@time.mins:`;
+		  unit = $localize`:@@time-since.mins.ago:mins ago`;
 	        break;
               case 'second':
-		unit = $localize`:@@time.seconds:`;
+		unit = $localize`:@@time-since.seconds.ago:seconds ago`;
                 if (document.body.clientWidth < 768)
-		  unit = $localize`:@@time.secs:`;
+		  unit = $localize`:@@time-since.secs.ago:secs ago`;
 	        break;
             }
             return counter + ' ' + unit;
