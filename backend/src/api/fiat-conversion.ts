a79cdf9b77dc722373dea4edb84f80810439b39b
tomat <tomat@blarg.se>
2021-01-28 00:38:48 +0100

    added SEK price to backend

--- backend/src/api/fiat-conversion.ts
+++ backend/src/api/fiat-conversion.ts
@@ -4,7 +4,9 @@ import { IConversionRates } from '../mempool.interfaces';
 
 class FiatConversion {
   private conversionRates: IConversionRates = {
-    'USD': 0
+    'USD': 0,
+    'SEK': 0,
+    // @todo add all currencies we have locales for, make sure they exist in getAllMarketPrices first
   };
   private ratesChangedCallback: ((rates: IConversionRates) => void) | undefined;
 
@@ -27,10 +29,11 @@ class FiatConversion {
   private async updateCurrency(): Promise<void> {
     try {
       const response = await axios.get('https://price.bisq.wiz.biz/getAllMarketPrices', { timeout: 10000 });
-      const usd = response.data.data.find((item: any) => item.currencyCode === 'USD');
-      this.conversionRates = {
-        'USD': usd.price,
-      };
+      const conversionRates = {};
+      response.data.data.find((item: any) => (Object.keys(this.conversionRates).includes(item.currencyCode))).forEach((item: any) => {
+        conversionRates[item.currencyCode] = item.price;
+      });
+      this.conversionRates = conversionRates;
       if (this.ratesChangedCallback) {
         this.ratesChangedCallback(this.conversionRates);
       }
