2298db17d4aaecb63a094f0934e531191d6d86e4
softsimon <softsimon@protonmail.com>
2021-02-27 04:19:56 +0700

    Bisq markets dashboard. Base views. WIP.

---
+++ frontend/src/app/bisq/bisq-dashboard/bisq-dashboard.component.ts
@@ -0,0 +1,43 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { BisqApiService } from '../bisq-api.service';
+
+@Component({
+  selector: 'app-bisq-dashboard',
+  templateUrl: './bisq-dashboard.component.html',
+  styleUrls: ['./bisq-dashboard.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BisqDashboardComponent implements OnInit {
+  tickers$: Observable<any>;
+
+  constructor(
+    private bisqApiService: BisqApiService,
+  ) { }
+
+  ngOnInit(): void {
+    this.tickers$ = combineLatest([
+      this.bisqApiService.getMarketsTicker$(),
+      this.bisqApiService.getMarkets$()
+    ])
+    .pipe(
+      map(([tickers, markets]) => {
+        const newTickers = [];
+        for (const t in tickers) {
+          tickers[t].pair_url = t;
+          tickers[t].pair = t.replace('_', '/').toUpperCase();
+          tickers[t].market = markets[t];
+          newTickers.push(tickers[t]);
+        }
+        console.log(newTickers);
+        return newTickers;
+      })
+    );
+  }
+
+  trackByFn(index: number) {
+    return index;
+  }
+
+}
