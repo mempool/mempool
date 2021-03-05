0eb0348b4e3649da8477a24b3693f1a3c52c552a
softsimon <softsimon@protonmail.com>
2021-03-05 19:03:57 +0700

    ToS page: Use bigger logo for hi res display.

--- frontend/src/app/components/terms-of-service/terms-of-service.component.ts
+++ frontend/src/app/components/terms-of-service/terms-of-service.component.ts
@@ -6,14 +6,9 @@ import { Env, StateService } from '../../services/state.service';
   templateUrl: './terms-of-service.component.html'
 })
 export class TermsOfServiceComponent {
-  env: Env;
   officialMempoolSpace = this.stateService.env.OFFICIAL_MEMPOOL_SPACE;
 
   constructor(
     private stateService: StateService,
   ) { }
-
-  ngOnInit() {
-    this.env = this.stateService.env;
-  }
 }
