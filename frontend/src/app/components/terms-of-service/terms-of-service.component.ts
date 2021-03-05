c7038c983efd2c998935e449a93d42418fbc8b4b
wiz <j@wiz.biz>
2021-03-05 16:53:28 +0900

    Update ToS for copyright, mempool.space operator, external link policy

--- frontend/src/app/components/terms-of-service/terms-of-service.component.ts
+++ frontend/src/app/components/terms-of-service/terms-of-service.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { Env, StateService } from '../../services/state.service';
 
 @Component({
   selector: 'app-terms-of-service',
   templateUrl: './terms-of-service.component.html'
 })
 export class TermsOfServiceComponent {
-  constructor() { }
+  env: Env;
+  officialMempoolSpace = this.stateService.env.OFFICIAL_MEMPOOL_SPACE;
+
+  constructor(
+    private stateService: StateService,
+  ) { }
+
+  ngOnInit() {
+    this.env = this.stateService.env;
+  }
 }
