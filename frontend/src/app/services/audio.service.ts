9929a6af185c7fb3e946810cd7c5e60fe0760f5c
Jonathan Underwood <junderwood@bitcoinbank.co.jp>
2021-03-12 15:12:27 +0900

    Maybe a little lower

--- frontend/src/app/services/audio.service.ts
+++ frontend/src/app/services/audio.service.ts
@@ -20,7 +20,7 @@ export class AudioService {
     this.isPlaying = true;
     this.audio.src = '../../../resources/sounds/' + name + '.mp3';
     this.audio.load();
-    this.audio.volume = 0.6; // 60% volume
+    this.audio.volume = 0.55; // 55% volume
     this.audio.play().catch((e) => {
       console.log('Play sound failed' + e);
     });
