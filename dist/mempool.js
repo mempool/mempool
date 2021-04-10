5003538319de0760b4adef4af66c4c0582917ac0
Miguel Medeiros <miguel.medeiros@gmail.com>
2021-04-10 11:31:16 -0300

    FIX: change build script to enable standalone

--- dist/mempool.js
+++ dist/mempool.js
@@ -653,15 +653,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var browserWS = function (options, defaultWs, websocketEndpoint) {
     var ws = new WebSocket(websocketEndpoint || defaultWs);
     ws.addEventListener('open', function open() {
-        ws.send(JSON.stringify({ action: 'init' }));
-        setInterval(function timeout() {
-            ws.send(JSON.stringify({
-                action: 'want',
-                data: options,
-            }));
-        }, 500);
+        handleMessage(ws, options);
     });
     return ws;
+};
+var handleMessage = function (ws, options) {
+    ws.send(JSON.stringify({ action: 'init' }));
+    setInterval(function timeout() {
+        ws.send(JSON.stringify({
+            action: 'want',
+            data: options,
+        }));
+    }, 500);
 };
 exports.default = browserWS;
 
