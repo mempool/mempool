f8f078a08f5922f13c0f38cddcb9428a10f93dce
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2021-05-21 12:03:18 -0300

    Fix missing whitespace linting. (#534)

--- frontend/src/app/components/transactions-list/transactions-list.component.ts
+++ frontend/src/app/components/transactions-list/transactions-list.component.ts
@@ -76,9 +76,9 @@ export class TransactionsListComponent implements OnInit, OnChanges {
   onScroll() {
     const scrollHeight = document.body.scrollHeight;
     const scrollTop = document.documentElement.scrollTop;
-    if(scrollHeight > 0){
+    if (scrollHeight > 0){
       const percentageScrolled = scrollTop * 100 / scrollHeight;
-      if(percentageScrolled > 90){
+      if (percentageScrolled > 90){
         this.loadMore.emit();
       }
     }
