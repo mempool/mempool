a103b430ae2e97914ec103c86644849fb6eeca87
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2021-07-01 15:14:08 -0300

    Trigger difficulty calculation every 5 seconds.

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -114,67 +114,10 @@ export class DashboardComponent implements OnInit {
       })
     );
 
-    this.difficultyEpoch$ = combineLatest([
-      this.stateService.blocks$.pipe(map(([block]) => block)),
-      this.stateService.lastDifficultyAdjustment$
-    ])
-    .pipe(
-      map(([block, DATime]) => {
-        const now = new Date().getTime() / 1000;
-        const diff = now - DATime;
-        const blocksInEpoch = block.height % 2016;
-        const estimatedBlocks = Math.round(diff / 60 / 10);
-        let difficultyChange = 0;
-        if (blocksInEpoch > 0) {
-          difficultyChange = (600 / (diff / blocksInEpoch ) - 1) * 100;
-        }
-
-        let base = 0;
-        let green = 0;
-        let red = 0;
-
-        if (blocksInEpoch >= estimatedBlocks) {
-          base = estimatedBlocks / 2016 * 100;
-          green = (blocksInEpoch - estimatedBlocks) / 2016 * 100;
-        } else {
-          base = blocksInEpoch / 2016 * 100;
-          red = Math.min((estimatedBlocks - blocksInEpoch) / 2016 * 100, 100 - base);
-        }
-
-        let colorAdjustments = '#dc3545';
-        if (difficultyChange >= 0) {
-          colorAdjustments = '#299435';
-        }
-
-        const timeAvgDiff = difficultyChange * 0.1;
-
-        let timeAvgMins = 10;
-        if (timeAvgDiff > 0 ){
-          timeAvgMins -= Math.abs(timeAvgDiff);
-        }else{
-          timeAvgMins += Math.abs(timeAvgDiff);
-        }
-
-        const remainingBlocks = 2016 - blocksInEpoch;
-        const nowMilliseconds = now * 1000;
-        const timeAvgMilliseconds = timeAvgMins * 60 * 1000;
-        const remainingBlocsMilliseconds = remainingBlocks * timeAvgMilliseconds;
-
-        return {
-          base: base + '%',
-          green: green + '%',
-          red: red + '%',
-          change: difficultyChange,
-          progress: base.toFixed(2),
-          remainingBlocks,
-          timeAvg: timeAvgMins.toFixed(0),
-          colorAdjustments,
-          blocksInEpoch,
-          newDifficultyHeight: block.height + remainingBlocks,
-          remaingTime: remainingBlocsMilliseconds + nowMilliseconds
-        };
-      })
-    );
+    this.calculateDifficultyAdjustment();
+    setInterval(() => {
+      this.calculateDifficultyAdjustment();
+    }, 5000);
 
     this.mempoolBlocksData$ = this.stateService.mempoolBlocks$
       .pipe(
@@ -279,5 +222,68 @@ export class DashboardComponent implements OnInit {
       this.collapseLevel = 'one';
     }
     this.storageService.setValue('dashboard-collapsed', this.collapseLevel);
+  }
+
+  calculateDifficultyAdjustment() {
+    this.difficultyEpoch$ = combineLatest([
+      this.stateService.blocks$.pipe(map(([block]) => block)),
+      this.stateService.lastDifficultyAdjustment$
+    ])
+    .pipe(
+      map(([block, DATime]) => {
+        const now = new Date().getTime() / 1000;
+        const diff = now - DATime;
+        const blocksInEpoch = block.height % 2016;
+        const estimatedBlocks = Math.round(diff / 60 / 10);
+        let difficultyChange = 0;
+        if (blocksInEpoch > 0) {
+          difficultyChange = (600 / (diff / blocksInEpoch ) - 1) * 100;
+        }
+
+        let base = 0;
+        let green = 0;
+        let red = 0;
+
+        if (blocksInEpoch >= estimatedBlocks) {
+          base = estimatedBlocks / 2016 * 100;
+          green = (blocksInEpoch - estimatedBlocks) / 2016 * 100;
+        } else {
+          base = blocksInEpoch / 2016 * 100;
+          red = Math.min((estimatedBlocks - blocksInEpoch) / 2016 * 100, 100 - base);
+        }
+
+        let colorAdjustments = '#dc3545';
+        if (difficultyChange >= 0) {
+          colorAdjustments = '#299435';
+        }
+
+        const timeAvgDiff = difficultyChange * 0.1;
+
+        let timeAvgMins = 10;
+        if (timeAvgDiff > 0 ){
+          timeAvgMins -= Math.abs(timeAvgDiff);
+        }else{
+          timeAvgMins += Math.abs(timeAvgDiff);
+        }
+
+        const remainingBlocks = 2016 - blocksInEpoch;
+        const nowMilliseconds = now * 1000;
+        const timeAvgMilliseconds = timeAvgMins * 60 * 1000;
+        const remainingBlocsMilliseconds = remainingBlocks * timeAvgMilliseconds;
+        return {
+          base: base + '%',
+          green: green + '%',
+          red: red + '%',
+          change: difficultyChange,
+          progress: base.toFixed(2),
+          remainingBlocks,
+          timeAvg: timeAvgMins.toFixed(0),
+          colorAdjustments,
+          blocksInEpoch,
+          newDifficultyHeight: block.height + remainingBlocks,
+          remaingTime: remainingBlocsMilliseconds + nowMilliseconds
+        };
+      })
+    );
   }
 }
