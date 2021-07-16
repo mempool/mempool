2b4017596274512fce97f8dc8a1b0b611d4b4190
github2k20 <priyansh.44rastogi@gmail.com>
2021-07-16 07:23:43 +0530

    Added link to API

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -592,7 +592,7 @@ class Routes {
       }
       res.status(500).send(e.message || e);
     }
-  }z
+  }
 
   public async getAddressTransactions(req: Request, res: Response) {
     if (config.MEMPOOL.BACKEND === 'none') {
@@ -667,12 +667,12 @@ class Routes {
     res.status(501).send('Not implemented');
   }
 
-  public async getDifficultyChange(req: Request, res: Response) {
+  public getDifficultyChange(req: Request, res: Response) {
     try {
       const now = new Date().getTime() / 1000;
       const DATime=blocks.getLastDifficultyAdjustmentTime();
       const diff = now - DATime;
-      const blockHeight=await bitcoinApi.$getBlockHeightTip();
+      const blockHeight=blocks.getCurrentBlockHeight();
       const blocksInEpoch = blockHeight % 2016;
       const estimatedBlocks = Math.round(diff / 60 / 10);
       const difficultyChange = (600 / (diff / blocksInEpoch ) - 1) * 100;
@@ -686,26 +686,20 @@ class Routes {
         timeAvgMins += Math.abs(timeAvgDiff);
       }
 
-      const remainingBlocks = estimatedBlocks - blocksInEpoch;
+      const remainingBlocks = 2016 - blocksInEpoch;
       const timeAvgSeconds = timeAvgMins * 60;
-      const remainingBlockSeconds = remainingBlocks * timeAvgSeconds;
-      const remainingTime=(remainingBlockSeconds + now);
-      const totalTime=remainingTime-DATime;
-      const progressPercent=100-((remainingBlockSeconds*100)/totalTime);
+      const remainingTime = remainingBlocks * timeAvgSeconds;
+      const expectedTime=(remainingTime + now);
+      const totalTime=expectedTime-DATime;
+      const progressPercent=100-((remainingTime*100)/totalTime);
 
       var date= new Date(0); // The 0 there is the key, which sets the date to the epoch
-      date.setUTCSeconds(remainingTime);
+      date.setUTCSeconds(expectedTime);
 
       const result={
-        now,
-        DATime,
-        diff,
-        blockHeight,
-        blocksInEpoch,
-        estimatedBlocks,
         difficultyChange,
         remainingBlocks,
-        remainingBlockSeconds,
+        remainingTime,
         date,
         progressPercent
       }
