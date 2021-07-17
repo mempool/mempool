cd94b814f5439bd2711b6dd9ff4425132a1ca7f6
Felipe Knorr Kuhn <git@knorrium.info>
2021-07-16 21:00:14 -0700

    improve bisq tests

--- frontend/cypress/integration/bisq/bisq.spec.ts
+++ frontend/cypress/integration/bisq/bisq.spec.ts
@@ -10,32 +10,41 @@ describe('Bisq', () => {
         cy.intercept('/bisq/api/txs/*/*').as('txs');
         cy.intercept('/bisq/api/blocks/*/*').as('blocks');
         cy.intercept('/bisq/api/stats').as('stats');
+        
+        Cypress.Commands.add('waitForDashboard', (label) => {
+            cy.wait('@socket');
+            cy.wait('@hloc');
+            cy.wait('@ticker');
+            cy.wait('@markets');
+            cy.wait('@7d');
+            cy.wait('@trades');
+          })
     });
-    it('loads the dashboard', () => {
-      cy.visit('/bisq');
 
-      cy.wait('@socket');
-      cy.wait('@hloc');
-      cy.wait('@ticker');
-      cy.wait('@markets');
-      cy.wait('@7d');
-      cy.wait('@trades');
+    it('loads the dashboard', () => {
+        cy.visit('/bisq');
+        cy.waitForDashboard();
     });
 
     it('loads the transactions screen', () => {
         cy.visit('/bisq');
+        cy.waitForDashboard();
         cy.get('li:nth-of-type(2) > a').click().then(() => {
-            cy.wait('@txs');
+            cy.get('.table > tr').should('have.length', 50);
         });
     });
+
     it('loads the blocks screen', () => {
         cy.visit('/bisq');
+        cy.waitForDashboard();
         cy.get('li:nth-of-type(3) > a').click().then(() => {
             cy.wait('@blocks');
         });
     });
+
     it('loads the stats screen', () => {
         cy.visit('/bisq');
+        cy.waitForDashboard();
         cy.get('li:nth-of-type(4) > a').click().then(() => {
             cy.wait('@stats');
         });
@@ -43,8 +52,11 @@ describe('Bisq', () => {
 
     it('loads the api screen', () => {
         cy.visit('/bisq');
+        cy.waitForDashboard();
         cy.get('li:nth-of-type(5) > a').click().then(() => {
-
+            cy.get('.card').should('have.length.at.least', 1);
+            cy.get('.card').first().click();
+            cy.get('.card-body');
         });
     });
 
