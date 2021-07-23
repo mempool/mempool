9c47810eff9496c615ac8aba6ed297a4b586bc6c
Felipe Knorr Kuhn <git@knorrium.info>
2021-07-23 15:38:49 -0700

    stabilize mainnet tests

--- frontend/cypress/integration/mainnet/mainnet.spec.ts
+++ frontend/cypress/integration/mainnet/mainnet.spec.ts
@@ -1,6 +1,10 @@
+import { delay } from "cypress/types/bluebird";
+import { emitMempoolInfo } from "../../support/websocket";
+
+
 describe('Mainnet', () => {
     beforeEach(() => {
-        cy.intercept('/sockjs-node/info*').as('socket');
+        //cy.intercept('/sockjs-node/info*').as('socket');
         cy.intercept('/api/block-height/*').as('block-height');
         cy.intercept('/api/block/*').as('block');
         cy.intercept('/api/block/*/txs/0').as('block-txs');
@@ -8,7 +12,6 @@ describe('Mainnet', () => {
         cy.intercept('/resources/pools.json').as('pools');
 
         Cypress.Commands.add('waitForBlockData', () => {
-            cy.wait('@socket');
             cy.wait('@tx-outspends');
             cy.wait('@pools');
           });
@@ -20,25 +23,30 @@ describe('Mainnet', () => {
     });
 
     it('loads the dashboard with the skeleton blocks', () => {
-      cy.visit('/');
-
-      cy.intercept('/sockjs-node/info/**', () => {
-        cy.get('#mempool-block-0').should('be.visible');
-        cy.get('#mempool-block-1').should('be.visible');
-        cy.get('#mempool-block-2').should('be.visible');
+        cy.mockMempoolSocket();
+        cy.visit("/");
         cy.get(':nth-child(1) > #bitcoin-block-0').should('be.visible');
         cy.get(':nth-child(2) > #bitcoin-block-0').should('be.visible');
         cy.get(':nth-child(3) > #bitcoin-block-0').should('be.visible');
-      });
-      cy.waitForSkeletonGone();
+        cy.get('#mempool-block-0').should('be.visible');
+        cy.get('#mempool-block-1').should('be.visible');
+        cy.get('#mempool-block-2').should('be.visible');
 
+        emitMempoolInfo({
+            'params': {
+              loaded: true
+            }
+        });
+        cy.get(':nth-child(1) > #bitcoin-block-0').should('not.exist');
+        cy.get(':nth-child(2) > #bitcoin-block-0').should('not.exist');
+        cy.get(':nth-child(3) > #bitcoin-block-0').should('not.exist');
     });
 
     it('loads the blocks screen', () => {
         cy.visit('/');
         cy.waitForSkeletonGone();
         cy.get('li:nth-of-type(2) > a').click().then(() => {
-           cy.wait(1000);
+            cy.waitForPageIdle();
         });
     });
 
@@ -57,7 +65,7 @@ describe('Mainnet', () => {
             cy.waitForSkeletonGone();
             cy.get('li:nth-of-type(4) > a').click().then(() => {
                 cy.viewport('macbook-16');
-                cy.wait(1000);
+                cy.get('.chart-holder');
                 cy.get('.blockchain-wrapper').should('be.visible');
                 cy.get('#mempool-block-0').should('be.visible');
                 //cy.get('#mempool-block-1').should('be.visible');
@@ -70,7 +78,7 @@ describe('Mainnet', () => {
             cy.waitForSkeletonGone();
             cy.get('li:nth-of-type(4) > a').click().then(() => {
                 cy.viewport('iphone-6');
-                cy.wait(1000);
+                cy.get('.chart-holder');
                 cy.get('.blockchain-wrapper').should('not.be.visible');
             });
         });
@@ -89,12 +97,14 @@ describe('Mainnet', () => {
         it('shows empty blocks properly', () => {
             cy.visit('/block/0000000000000000000bd14f744ef2e006e61c32214670de7eb891a5732ee775');
             cy.waitForSkeletonGone();
+            cy.waitForPageIdle();
             cy.get('h2').invoke('text').should('equal', '1 transaction');
         });
 
         it('expands and collapses the block details', () => {
             cy.visit('/block/0');
             cy.waitForSkeletonGone();
+            cy.waitForPageIdle();
             cy.get('.btn.btn-outline-info').click().then(() => {
                 cy.get('#details').should('be.visible');
             });
@@ -106,6 +116,7 @@ describe('Mainnet', () => {
         it('shows blocks with no pagination', () => {
             cy.visit('/block/00000000000000000001ba40caf1ad4cec0ceb77692662315c151953bfd7c4c4');
             cy.waitForSkeletonGone();
+            cy.waitForPageIdle();
             cy.get('.block-tx-title h2').invoke('text').should('equal', '19 transactions');
             cy.get('.pagination-container ul.pagination').first().children().should('have.length', 5);
         });
@@ -117,6 +128,7 @@ describe('Mainnet', () => {
             cy.get('.pagination-container a').invoke('text').then((text1) => {
                 cy.get('.active + li').first().click().then(() => {
                     cy.waitForSkeletonGone();
+                    cy.waitForPageIdle();
                     cy.get('.header-bg.box > a').invoke('text').then((text2) => {
                         expect(text1).not.to.eq(text2);
                     });
@@ -128,6 +140,7 @@ describe('Mainnet', () => {
             cy.viewport(760, 800);
             cy.visit('/block/000000000000000000049281946d26fcba7d99fdabc1feac524bc3a7003d69b3').then(() => {
                 cy.waitForSkeletonGone();
+                cy.waitForPageIdle();
             });
             
             // 5 pages + 4 buttons = 9 buttons
@@ -138,6 +151,7 @@ describe('Mainnet', () => {
             cy.viewport(669, 800);
             cy.visit('/block/000000000000000000049281946d26fcba7d99fdabc1feac524bc3a7003d69b3').then(() => {
                 cy.waitForSkeletonGone();
+                cy.waitForPageIdle();
             });
             
             // 3 pages + 4 buttons = 7 buttons
