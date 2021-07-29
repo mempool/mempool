3536c73192642f9f82496b7144cf9275e2af8438
muirronk <88157188+muirronk@users.noreply.github.com>
2021-07-29 09:48:11 -0700

    remove extra empty line

--- frontend/cypress/integration/liquid/liquid.spec.ts
+++ frontend/cypress/integration/liquid/liquid.spec.ts
@@ -91,7 +91,6 @@ describe('Liquid', () => {
         });
     });
 
-
     describe('unblinded TX', () => {
         it('show unblinded TX', () => {
             cy.visit('/liquid/tx/f2f41c0850e8e7e3f1af233161fd596662e67c11ef10ed15943884186fbb7f46#blinded=100000,6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d,0ab9f70650f16b1db8dfada05237f7d0d65191c3a13183da8a2ddddfbde9a2ad,fd98b2edc5530d76acd553f206a431f4c1fab27e10e290ad719582af878e98fc,2364760,6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d,90c7a43b15b905bca045ca42a01271cfe71d2efe3133f4197792c24505cb32ed,12eb5959d9293b8842e7dd8bc9aa9639fd3fd031c5de3ba911adeca94eb57a3a');
