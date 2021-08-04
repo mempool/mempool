d841933b2125e3e36ee8ed6c2cd95ea9d8b39071
softsimon <softsimon@protonmail.com>
2021-08-05 01:31:22 +0300

    Sync blockstream asset registry when in liquid base module mode.

--- frontend/sync-assets.js
+++ frontend/sync-assets.js
@@ -1,9 +1,24 @@
 var https = require('https');
 var fs = require('fs');
 
+const CONFIG_FILE_NAME = 'mempool-frontend-config.json';
+let configContent = {};
+
 var PATH = 'dist/mempool/browser/en-US/resources/';
 if (process.argv[2] && process.argv[2] === 'dev') {
   PATH = 'src/resources/';
+}
+
+try {
+  const rawConfig = fs.readFileSync(CONFIG_FILE_NAME);
+  configContent = JSON.parse(rawConfig);
+  console.log(`${CONFIG_FILE_NAME} file found, using provided config`);
+} catch (e) {
+  if (e.code !== 'ENOENT') {
+    throw new Error(e);
+  } else {
+    console.log(`${CONFIG_FILE_NAME} file not found, using default config`);
+  }
 }
 
 function download(filename, url) {
@@ -18,9 +33,18 @@ function download(filename, url) {
   });
 }
 
+const poolsJsonUrl = 'https://raw.githubusercontent.com/btccom/Blockchain-Known-Pools/master/pools.json';
+let assetsJsonUrl = 'https://raw.githubusercontent.com/mempool/asset_registry_db/master/index.json';
+let assetsMinimalJsonUrl = 'https://raw.githubusercontent.com/mempool/asset_registry_db/master/index.minimal.json';
+
+if (configContent.BASE_MODULE && configContent.BASE_MODULE === 'liquid') {
+  assetsJsonUrl = 'https://raw.githubusercontent.com/Blockstream/asset_registry_db/master/index.json';
+  assetsMinimalJsonUrl = 'https://raw.githubusercontent.com/Blockstream/asset_registry_db/master/index.minimal.json';
+}
+
 console.log('Downloading assets');
-download(PATH + 'assets.json', 'https://raw.githubusercontent.com/mempool/asset_registry_db/master/index.json');
+download(PATH + 'assets.json', assetsJsonUrl);
 console.log('Downloading assets minimal');
-download(PATH + 'assets.minimal.json', 'https://raw.githubusercontent.com/mempool/asset_registry_db/master/index.minimal.json');
+download(PATH + 'assets.minimal.json', assetsMinimalJsonUrl);
 console.log('Downloading mining pools info');
-download(PATH + 'pools.json', 'https://raw.githubusercontent.com/btccom/Blockchain-Known-Pools/master/pools.json');
+download(PATH + 'pools.json', poolsJsonUrl);
