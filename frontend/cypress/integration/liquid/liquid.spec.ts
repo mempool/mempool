de5518d26215f04d1d73584866114adbf174ea6c
Felipe Knorr Kuhn <100320+knorrium@users.noreply.github.com>
2021-08-17 15:53:57 -0700

    update test specs to handle the new multisite setup on CI

--- frontend/cypress/integration/liquid/liquid.spec.ts
+++ frontend/cypress/integration/liquid/liquid.spec.ts
@@ -1,5 +1,8 @@
 describe('Liquid', () => {
+    let baseModule;
     beforeEach(() => {
+        baseModule = (Cypress.env('BASE_MODULE') && Cypress.env('BASE_MODULE') === 'liquid') ? '' : '/liquid';
+
         cy.intercept('/liquid/api/block/**').as('block');
         cy.intercept('/liquid/api/blocks/').as('blocks');
         cy.intercept('/liquid/api/tx/**/outspends').as('outspends');
@@ -13,127 +16,125 @@ describe('Liquid', () => {
           });
     });
 
-    it('loads the dashboard', () => {
-        cy.visit('/liquid');
-        cy.waitForSkeletonGone();
-    });
+    if (Cypress.env("BASE_MODULE") === '' || Cypress.env("BASE_MODULE") !== 'bisq') {
 
-    it('loads the blocks page', () => {
-        cy.visit('/liquid/blocks');
-        cy.waitForSkeletonGone();
-    });
-
-    it('loads a specific block page', () => {
-        cy.visit('/liquid/block/7e1369a23a5ab861e7bdede2aadcccae4ea873ffd9caf11c7c5541eb5bcdff54');
-        cy.waitForSkeletonGone();
-    });
-
-    it('loads the graphs page', () => {
-        cy.visit('/liquid/graphs');
-        cy.waitForSkeletonGone();
-    });
-
-    it('loads the tv page - desktop', () => {
-        cy.visit('/liquid');
-        cy.waitForSkeletonGone();
-        cy.get('li:nth-of-type(3) > a').click().then(() => {
-            cy.wait(1000);
-        });
-    });
-
-    it('loads the graphs page - mobile', () => {
-        cy.visit('/liquid');
-        cy.waitForSkeletonGone();
-        cy.get('li:nth-of-type(3) > a').click().then(() => {
-            cy.viewport('iphone-6');
-            cy.wait(1000);
-            cy.get('.tv-only').should('not.exist');
-        });
-    });
-
-    describe('assets', () => {
-        it('shows the assets screen', () => {
-            cy.visit('/liquid');
+        it('loads the dashboard', () => {
+            cy.visit(`${baseModule}`);
             cy.waitForSkeletonGone();
-            cy.get('li:nth-of-type(5) > a').click().then(() => {
-                cy.get('table tr').should('have.length', 5);
+        });
+
+        it('loads the blocks page', () => {
+            cy.visit(`${baseModule}/blocks`);
+            cy.waitForSkeletonGone();
+        });
+
+        it('loads a specific block page', () => {
+            cy.visit(`${baseModule}/block/7e1369a23a5ab861e7bdede2aadcccae4ea873ffd9caf11c7c5541eb5bcdff54`);
+            cy.waitForSkeletonGone();
+        });
+
+        it('loads the graphs page', () => {
+            cy.visit(`${baseModule}/graphs`);
+            cy.waitForSkeletonGone();
+        });
+
+        it('loads the tv page - desktop', () => {
+            cy.visit(`${baseModule}`);
+            cy.waitForSkeletonGone();
+            cy.get('li:nth-of-type(3) > a').click().then(() => {
+                cy.wait(1000);
             });
         });
 
-        it('allows searching assets', () => {
-            cy.visit('/liquid');
+        it('loads the graphs page - mobile', () => {
+            cy.visit(`${baseModule}`)
             cy.waitForSkeletonGone();
-            cy.get('li:nth-of-type(5) > a').click().then(() => {
+            cy.get('li:nth-of-type(3) > a').click().then(() => {
+                cy.viewport('iphone-6');
+                cy.wait(1000);
+                cy.get('.tv-only').should('not.exist');
+            });
+        });
+
+        describe('assets', () => {
+            it('shows the assets screen', () => {
+                cy.visit(`${baseModule}/assets`);
+                cy.waitForSkeletonGone();
+                cy.get('table tr').should('have.length', 5);
+            });
+
+            it('allows searching assets', () => {
+                cy.visit(`${baseModule}/assets`);
+                cy.waitForSkeletonGone();
                 cy.get('.container-xl input').click().type('Liquid Bitcoin').then(() => {
                     cy.get('table tr').should('have.length', 1);
                 });
             });
-        });
 
-        it('shows a specific asset ID', () => {
-            cy.visit('/liquid');
-            cy.waitForSkeletonGone();
-            cy.get('li:nth-of-type(5) > a').click().then(() => {
+            it('shows a specific asset ID', () => {
+                cy.visit(`${baseModule}/assets`);
+                cy.waitForSkeletonGone();
                 cy.get('.container-xl input').click().type('Liquid CAD').then(() => {
                     cy.get('table tr td:nth-of-type(1) a').click();
                 });
             });
         });
-    });
 
 
-    describe('unblinded TX', () => {
-        it('show unblinded TX', () => {
-            cy.visit('/liquid/tx/f2f41c0850e8e7e3f1af233161fd596662e67c11ef10ed15943884186fbb7f46#blinded=100000,6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d,0ab9f70650f16b1db8dfada05237f7d0d65191c3a13183da8a2ddddfbde9a2ad,fd98b2edc5530d76acd553f206a431f4c1fab27e10e290ad719582af878e98fc,2364760,6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d,90c7a43b15b905bca045ca42a01271cfe71d2efe3133f4197792c24505cb32ed,12eb5959d9293b8842e7dd8bc9aa9639fd3fd031c5de3ba911adeca94eb57a3a');
-            cy.waitForSkeletonGone();
-            cy.get('#table-tx-vin tr').should('have.class', 'assetBox');
-            cy.get('#table-tx-vout tr').should('have.class', 'assetBox');
+        describe('unblinded TX', () => {
+            it('show unblinded TX', () => {
+                cy.visit(`${baseModule}/tx/f2f41c0850e8e7e3f1af233161fd596662e67c11ef10ed15943884186fbb7f46#blinded=100000,6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d,0ab9f70650f16b1db8dfada05237f7d0d65191c3a13183da8a2ddddfbde9a2ad,fd98b2edc5530d76acd553f206a431f4c1fab27e10e290ad719582af878e98fc,2364760,6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d,90c7a43b15b905bca045ca42a01271cfe71d2efe3133f4197792c24505cb32ed,12eb5959d9293b8842e7dd8bc9aa9639fd3fd031c5de3ba911adeca94eb57a3a`);
+                cy.waitForSkeletonGone();
+                cy.get('#table-tx-vin tr').should('have.class', 'assetBox');
+                cy.get('#table-tx-vout tr').should('have.class', 'assetBox');
+            });
+
+            it('show empty unblinded TX', () => {
+                cy.visit(`${baseModule}/tx/f2f41c0850e8e7e3f1af233161fd596662e67c11ef10ed15943884186fbb7f46#blinded=`);
+                cy.waitForSkeletonGone();
+                cy.get('#table-tx-vin tr').should('have.class', '');
+                cy.get('#table-tx-vout tr').should('have.class', '');
+            });
+
+            it('show invalid unblinded TX hex', () => {
+                cy.visit(`${baseModule}/tx/f2f41c0850e8e7e3f1af233161fd596662e67c11ef10ed15943884186fbb7f46#blinded=123`);
+                cy.waitForSkeletonGone();
+                cy.get('#table-tx-vin tr').should('have.class', '');
+                cy.get('#table-tx-vout tr').should('have.class', '');
+                cy.get('.error-unblinded' ).contains('Error: Invalid blinding data (invalid hex)');
+            });
+
+            it('show first unblinded vout', () => {
+                cy.visit(`${baseModule}/tx/f2f41c0850e8e7e3f1af233161fd596662e67c11ef10ed15943884186fbb7f46#blinded=100000,6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d,0ab9f70650f16b1db8dfada05237f7d0d65191c3a13183da8a2ddddfbde9a2ad,fd98b2edc5530d76acd553f206a431f4c1fab27e10e290ad719582af878e98fc`);
+                cy.waitForSkeletonGone();
+                cy.get('#table-tx-vout tr:first-child()').should('have.class', 'assetBox');
+            });
+
+            it('show second unblinded vout', () => {
+                cy.visit(`${baseModule}/tx/f2f41c0850e8e7e3f1af233161fd596662e67c11ef10ed15943884186fbb7f46#blinded=2364760,6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d,90c7a43b15b905bca045ca42a01271cfe71d2efe3133f4197792c24505cb32ed,12eb5959d9293b8842e7dd8bc9aa9639fd3fd031c5de3ba911adeca94eb57a3a`);
+                cy.get('#table-tx-vout tr').should('have.class', 'assetBox');
+            });
+
+            it('show invalid error unblinded TX', () => {
+                cy.visit(`${baseModule}/tx/f2f41c0850e8e7e3f1af233161fd596662e67c11ef10ed15943884186fbb7f46#blinded=100000,6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d,0ab9f70650f16b1db8dfada05237f7d0d65191c3a13183da8a2ddddfbde9a2ad,fd98b2edc5530d76acd553f206a431f4c1fab27e10e290ad719582af878e98fc,2364760,6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d,90c7a43b15b905bca045ca42a01271cfe71d2efe3133f4197792c24505cb32ed,12eb5959d9293b8842e7dd8bc9aa9639fd3fd031c5de3ba911adeca94eb57a3c`);
+                cy.waitForSkeletonGone();
+                cy.get('#table-tx-vout tr').should('have.class', 'assetBox');
+                cy.get('.error-unblinded' ).contains('Error: Invalid blinding data.');
+            });
+
+            it('shows asset peg in/out and burn transactions', () => {
+                cy.visit(`${baseModule}/asset/6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d`);
+                cy.waitForSkeletonGone();
+                cy.get('#table-tx-vout tr').not('.assetBox');
+                cy.get('#table-tx-vin tr').not('.assetBox');
+            });
+
+            it('prevents regressing issue #644', () => {
+                cy.visit(`${baseModule}/tx/393b890966f305e7c440fcfb12a13f51a7a9011cc59ff5f14f6f93214261bd82`);
+                cy.waitForSkeletonGone();
+            });
         });
-
-        it('show empty unblinded TX', () => {
-            cy.visit('/liquid/tx/f2f41c0850e8e7e3f1af233161fd596662e67c11ef10ed15943884186fbb7f46#blinded=');
-            cy.waitForSkeletonGone();
-            cy.get('#table-tx-vin tr').should('have.class', '');
-            cy.get('#table-tx-vout tr').should('have.class', '');
-        });
-
-        it('show invalid unblinded TX hex', () => {
-            cy.visit('/liquid/tx/f2f41c0850e8e7e3f1af233161fd596662e67c11ef10ed15943884186fbb7f46#blinded=123');
-            cy.waitForSkeletonGone();
-            cy.get('#table-tx-vin tr').should('have.class', '');
-            cy.get('#table-tx-vout tr').should('have.class', '');
-            cy.get('.error-unblinded' ).contains('Error: Invalid blinding data (invalid hex)');
-        });
-
-        it('show first unblinded vout', () => {
-            cy.visit('/liquid/tx/f2f41c0850e8e7e3f1af233161fd596662e67c11ef10ed15943884186fbb7f46#blinded=100000,6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d,0ab9f70650f16b1db8dfada05237f7d0d65191c3a13183da8a2ddddfbde9a2ad,fd98b2edc5530d76acd553f206a431f4c1fab27e10e290ad719582af878e98fc');
-            cy.waitForSkeletonGone();
-            cy.get('#table-tx-vout tr:first-child()').should('have.class', 'assetBox');
-        });
-
-        it('show second unblinded vout', () => {
-            cy.visit('/liquid/tx/f2f41c0850e8e7e3f1af233161fd596662e67c11ef10ed15943884186fbb7f46#blinded=2364760,6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d,90c7a43b15b905bca045ca42a01271cfe71d2efe3133f4197792c24505cb32ed,12eb5959d9293b8842e7dd8bc9aa9639fd3fd031c5de3ba911adeca94eb57a3a');
-            cy.get('#table-tx-vout tr').should('have.class', 'assetBox');
-        });
-
-        it('show invalid error unblinded TX', () => {
-            cy.visit('/liquid/tx/f2f41c0850e8e7e3f1af233161fd596662e67c11ef10ed15943884186fbb7f46#blinded=100000,6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d,0ab9f70650f16b1db8dfada05237f7d0d65191c3a13183da8a2ddddfbde9a2ad,fd98b2edc5530d76acd553f206a431f4c1fab27e10e290ad719582af878e98fc,2364760,6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d,90c7a43b15b905bca045ca42a01271cfe71d2efe3133f4197792c24505cb32ed,12eb5959d9293b8842e7dd8bc9aa9639fd3fd031c5de3ba911adeca94eb57a3c');
-            cy.waitForSkeletonGone();
-            cy.get('#table-tx-vout tr').should('have.class', 'assetBox');
-            cy.get('.error-unblinded' ).contains('Error: Invalid blinding data.');
-        });
-
-        it('shows asset peg in/out and burn transactions', () => {
-            cy.visit('/liquid/asset/6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d');
-            cy.waitForSkeletonGone();
-            cy.get('#table-tx-vout tr').not('.assetBox');
-            cy.get('#table-tx-vin tr').not('.assetBox');
-        });
-
-        it('prevents regressing issue #644', () => {
-            cy.visit('/liquid/tx/393b890966f305e7c440fcfb12a13f51a7a9011cc59ff5f14f6f93214261bd82');
-            cy.waitForSkeletonGone();
-        });
-    });
-
+    } else {
+        it.skip("Tests cannot be run on the selected BASE_MODULE");
+    }
 });
--- frontend/cypress/integration/testnet/testnet.spec.ts
+++ frontend/cypress/integration/testnet/testnet.spec.ts
@@ -1,4 +1,4 @@
-import { emitMempoolInfo } from "../../support/websocket";
+import { confirmAddress, emitMempoolInfo, sendWsMock, showNewTx, startTrackingAddress } from "../../support/websocket";
 
 describe('Testnet', () => {
   beforeEach(() => {
@@ -8,115 +8,121 @@ describe('Testnet', () => {
     cy.intercept('/api/tx/*/outspends').as('tx-outspends');
   });
 
-  it('loads the dashboard', () => {
-    cy.visit('/testnet');
-    cy.waitForSkeletonGone();
-  });
+  if (Cypress.env("BASE_MODULE") === '' || Cypress.env("BASE_MODULE") === 'mempool') {
 
-  it('loads the dashboard with the skeleton blocks', () => {
-    cy.mockMempoolSocket();
-    cy.visit("/signet");
-    cy.get(':nth-child(1) > #bitcoin-block-0').should('be.visible');
-    cy.get(':nth-child(2) > #bitcoin-block-0').should('be.visible');
-    cy.get(':nth-child(3) > #bitcoin-block-0').should('be.visible');
-    cy.get('#mempool-block-0').should('be.visible');
-    cy.get('#mempool-block-1').should('be.visible');
-    cy.get('#mempool-block-2').should('be.visible');
-
-    emitMempoolInfo({
-        'params': {
-          loaded: true
-        }
+    it('loads the dashboard', () => {
+        cy.visit('/testnet');
+        cy.waitForSkeletonGone();
     });
-    cy.get(':nth-child(1) > #bitcoin-block-0').should('not.exist');
-    cy.get(':nth-child(2) > #bitcoin-block-0').should('not.exist');
-    cy.get(':nth-child(3) > #bitcoin-block-0').should('not.exist');
-});
 
-  it('loads the blocks screen', () => {
-      cy.visit('/testnet');
-      cy.waitForSkeletonGone();
-      cy.get('li:nth-of-type(2) > a').click().then(() => {
-         cy.wait(1000);
-      });
-  });
+    it('loads the dashboard with the skeleton blocks', () => {
+        cy.mockMempoolSocket();
+        cy.visit("/testnet");
+        cy.get(':nth-child(1) > #bitcoin-block-0').should('be.visible');
+        cy.get(':nth-child(2) > #bitcoin-block-0').should('be.visible');
+        cy.get(':nth-child(3) > #bitcoin-block-0').should('be.visible');
+        cy.get('#mempool-block-0').should('be.visible');
+        cy.get('#mempool-block-1').should('be.visible');
+        cy.get('#mempool-block-2').should('be.visible');
 
-  it('loads the graphs screen', () => {
-      cy.visit('/testnet');
-      cy.waitForSkeletonGone();
-      cy.get('li:nth-of-type(3) > a').click().then(() => {
-          cy.wait(1000);
-      });
-  });
+        emitMempoolInfo({
+            'params': {
+            loaded: true
+            }
+        });
 
-  describe('tv mode', () => {
-      it('loads the tv screen - desktop', () => {
-          cy.viewport('macbook-16');
-          cy.visit('/testnet');
-          cy.waitForSkeletonGone();
-          cy.get('li:nth-of-type(4) > a').click().then(() => {
-              cy.wait(1000);
-              cy.get('.tv-only').should('not.exist');
-          });
-      });
-
-      it('loads the tv screen - mobile', () => {
-          cy.visit('/testnet');
-          cy.waitForSkeletonGone();
-          cy.get('li:nth-of-type(4) > a').click().then(() => {
-              cy.viewport('iphone-6');
-              cy.wait(1000);
-              cy.get('.tv-only').should('not.exist');
-          });
-      });
-  });
-
-
-  it('loads the api screen', () => {
-      cy.visit('/testnet');
-      cy.waitForSkeletonGone();
-      cy.get('li:nth-of-type(5) > a').click().then(() => {
-          cy.wait(1000);
-      });
-  });
-
-  describe('blocks', () => {
-      it('shows empty blocks properly', () => {
-          cy.visit('/testnet/block/0');
-          cy.waitForSkeletonGone();
-          cy.get('h2').invoke('text').should('equal', '1 transaction');
-      });
-
-      it('expands and collapses the block details', () => {
-          cy.visit('/testnet/block/0');
-          cy.waitForSkeletonGone();
-          cy.get('.btn.btn-outline-info').click().then(() => {
-              cy.get('#details').should('be.visible');
-          });
-
-          cy.get('.btn.btn-outline-info').click().then(() => {
-              cy.get('#details').should('not.be.visible');
-          });
-      });
-
-      it('shows blocks with no pagination', () => {
-          cy.visit('/testnet/block/000000000000002f8ce27716e74ecc7ad9f7b5101fed12d09e28bb721b9460ea');
-          cy.waitForSkeletonGone();
-          cy.get('h2').invoke('text').should('equal', '11 transactions');
-          cy.get('ul.pagination').first().children().should('have.length', 5);
-      });
-
-      it('supports pagination on the block screen', () => {
-          // 48 txs
-          cy.visit('/testnet/block/000000000000002ca3878ebd98b313a1c2d531f2e70a6575d232ca7564dea7a9');
-          cy.waitForSkeletonGone();
-          cy.get('.header-bg.box > a').invoke('text').then((text1) => {
-              cy.get('.active + li').first().click().then(() => {
-                  cy.get('.header-bg.box > a').invoke('text').then((text2) => {
-                      expect(text1).not.to.eq(text2);
-                  });
-              });
-          });
-      });
+        cy.get(':nth-child(1) > #bitcoin-block-0').should('not.exist');
+        cy.get(':nth-child(2) > #bitcoin-block-0').should('not.exist');
+        cy.get(':nth-child(3) > #bitcoin-block-0').should('not.exist');
     });
+
+    it('loads the blocks screen', () => {
+        cy.visit('/testnet');
+        cy.waitForSkeletonGone();
+        cy.get('li:nth-of-type(2) > a').click().then(() => {
+            cy.wait(1000);
+        });
+    });
+
+    it('loads the graphs screen', () => {
+        cy.visit('/testnet');
+        cy.waitForSkeletonGone();
+        cy.get('li:nth-of-type(3) > a').click().then(() => {
+            cy.wait(1000);
+        });
+    });
+
+    describe('tv mode', () => {
+        it('loads the tv screen - desktop', () => {
+            cy.viewport('macbook-16');
+            cy.visit('/testnet');
+            cy.waitForSkeletonGone();
+            cy.get('li:nth-of-type(4) > a').click().then(() => {
+                cy.wait(1000);
+                cy.get('.tv-only').should('not.exist');
+            });
+        });
+
+        it('loads the tv screen - mobile', () => {
+            cy.visit('/testnet');
+            cy.waitForSkeletonGone();
+            cy.get('li:nth-of-type(4) > a').click().then(() => {
+                cy.viewport('iphone-6');
+                cy.wait(1000);
+                cy.get('.tv-only').should('not.exist');
+            });
+        });
+    });
+
+
+    it('loads the api screen', () => {
+        cy.visit('/testnet');
+        cy.waitForSkeletonGone();
+        cy.get('li:nth-of-type(5) > a').click().then(() => {
+            cy.wait(1000);
+        });
+    });
+
+    describe('blocks', () => {
+        it('shows empty blocks properly', () => {
+            cy.visit('/testnet/block/0');
+            cy.waitForSkeletonGone();
+            cy.get('h2').invoke('text').should('equal', '1 transaction');
+        });
+
+        it('expands and collapses the block details', () => {
+            cy.visit('/testnet/block/0');
+            cy.waitForSkeletonGone();
+            cy.get('.btn.btn-outline-info').click().then(() => {
+                cy.get('#details').should('be.visible');
+            });
+
+            cy.get('.btn.btn-outline-info').click().then(() => {
+                cy.get('#details').should('not.be.visible');
+            });
+        });
+
+        it('shows blocks with no pagination', () => {
+            cy.visit('/testnet/block/000000000000002f8ce27716e74ecc7ad9f7b5101fed12d09e28bb721b9460ea');
+            cy.waitForSkeletonGone();
+            cy.get('h2').invoke('text').should('equal', '11 transactions');
+            cy.get('ul.pagination').first().children().should('have.length', 5);
+        });
+
+        it('supports pagination on the block screen', () => {
+            // 48 txs
+            cy.visit('/testnet/block/000000000000002ca3878ebd98b313a1c2d531f2e70a6575d232ca7564dea7a9');
+            cy.waitForSkeletonGone();
+            cy.get('.header-bg.box > a').invoke('text').then((text1) => {
+                cy.get('.active + li').first().click().then(() => {
+                    cy.get('.header-bg.box > a').invoke('text').then((text2) => {
+                        expect(text1).not.to.eq(text2);
+                    });
+                });
+            });
+        });
+    });
+    } else {
+        it.skip("Tests cannot be run on the selected BASE_MODULE");
+    }
   });
