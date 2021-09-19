605c1a980cf76e708c895e9d86cbfcbe78aa958c
softsimon <softsimon@protonmail.com>
2021-09-19 13:17:11 +0400

    Always use local hostname for API examples.

--- frontend/src/app/components/api-docs/api-docs.component.ts
+++ frontend/src/app/components/api-docs/api-docs.component.ts
@@ -40,18 +40,6 @@ export class ApiDocsComponent implements OnInit {
 
     this.hostname = `${document.location.protocol}//${this.hostname}`;
 
-    if (document.location.hostname === 'localhost') {
-      if (this.env.BASE_MODULE === 'bisq') {
-        this.hostname = `https://bisq.markets`;
-      }
-      if (this.env.BASE_MODULE === 'liquid') {
-        this.hostname = `https://liquid.network`;
-      }
-      if (this.env.BASE_MODULE === 'mempool') {
-        this.hostname = `https://mempool.space`;
-      }
-    }
-
     this.code = {
       address: {
         codeTemplate: {
@@ -2063,7 +2051,7 @@ export class ApiDocsComponent implements OnInit {
       },
       transactionCpfp: {
         codeTemplate: {
-          curl: `/api/fees/cpfp/%{1}`,
+          curl: `/api/v1/cpfp/%{1}`,
           commonJS: `
         const { %{0}: { fees } } = mempoolJS();
 
