5dab44e6c40368e46449fdcc68aeef8fea528104
Felipe Knorr Kuhn <git@knorrium.info>
2021-10-27 20:49:49 -0700

    Cleanup Cypress config and commands

--- frontend/cypress/plugins/index.js
+++ frontend/cypress/plugins/index.js
@@ -5,8 +5,7 @@ const CONFIG_FILE = 'mempool-frontend-config.json';
 module.exports = (on, config) => {
   if (fs.existsSync(CONFIG_FILE)) {
     let contents = JSON.parse(fs.readFileSync(CONFIG_FILE, 'utf8'));
-    console.log(contents);
-    config.env.BASE_MODULE = contents.BASE_MODULE;
+    config.env.BASE_MODULE = contents.BASE_MODULE ? contents.BASE_MODULE : 'mempool';
   } else {
     config.env.BASE_MODULE = 'mempool';
   }
