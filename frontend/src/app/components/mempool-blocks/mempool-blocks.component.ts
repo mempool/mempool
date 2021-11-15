3bab50a43bb86d732685dad9484fe8ca533629ad
Miguel Medeiros <miguel@miguelmedeiros.com.br>
2021-11-15 16:38:28 -0300

    fix: blocksfull variable.

--- frontend/src/app/components/mempool-blocks/mempool-blocks.component.ts
+++ frontend/src/app/components/mempool-blocks/mempool-blocks.component.ts
@@ -89,15 +89,17 @@ export class MempoolBlocksComponent implements OnInit, OnDestroy {
             }
           });
 
-          let blocks = mempoolBlocks;
           if (!mempoolBlocks.length) {
-            blocks = [{ index: 0, blockSize: 0, blockVSize: 0, feeRange: [0, 0], medianFee: 0, nTx: 0, totalFees: 0 }];
+            const emptyBlock = [{ index: 0, blockSize: 0, blockVSize: 0, feeRange: [0, 0], medianFee: 0, nTx: 0, totalFees: 0 }];
+            this.mempoolBlocks = this.reduceMempoolBlocksToFitScreen(emptyBlock);
+          } else {
+            const stringifiedBlocks = JSON.stringify(mempoolBlocks);
+            this.mempoolBlocksFull = JSON.parse(stringifiedBlocks);
+            this.mempoolBlocks = this.reduceMempoolBlocksToFitScreen(JSON.parse(stringifiedBlocks));
           }
-          this.mempoolBlocksFull = blocks;
-          this.mempoolBlocks = this.reduceMempoolBlocksToFitScreen(blocks);
           this.updateMempoolBlockStyles();
           this.calculateTransactionPosition();
-          return blocks;
+          return this.mempoolBlocks;
         })
       );
 
