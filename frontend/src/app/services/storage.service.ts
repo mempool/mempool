aee319ed518f3673c5cf2e0a6f4f691ddec8a08a
nymkappa <vialjoris@hotmail.com>
2021-12-13 11:56:24 +0900

    Initialize graphWindowPreference in localstorage properly

--- frontend/src/app/services/storage.service.ts
+++ frontend/src/app/services/storage.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
+  constructor(private router: Router, private route: ActivatedRoute) {
+    let graphWindowPreference: string = this.getValue('graphWindowPreference');
+    if (graphWindowPreference === null) { // First visit to mempool.space
+      if (this.router.url.includes("graphs")) {
+        this.setValue('graphWindowPreference', this.route.snapshot.fragment ? this.route.snapshot.fragment : "2h");
+      } else {
+        this.setValue('graphWindowPreference', "2h");
+      }
+    } else if (this.router.url.includes("graphs")) { // Visit a different graphs#fragment from last visit
+        if (this.route.snapshot.fragment !== null && graphWindowPreference !== this.route.snapshot.fragment) {
+          this.setValue('graphWindowPreference', this.route.snapshot.fragment);
+        }
+      }
+  }
+
   getValue(key: string): string {
     try {
       return localStorage.getItem(key);
