03b78cba42e9447a472c3d5b38be9362cbefcf21
hunicus <93150691+hunicus@users.noreply.github.com>
2021-12-17 07:00:48 -0500

    Add various ux improvements for mobile doc menu

--- frontend/src/app/components/docs/api-docs-nav.component.ts
+++ frontend/src/app/components/docs/api-docs-nav.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ApiDocsNavComponent implements OnInit {
 
   @Input() network: any;
+  @Input() collapseItem: any;
 
   constructor() { }
 
