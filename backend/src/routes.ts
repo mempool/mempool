f8f9108ae1b41d98ec12a5ce0d27e0bcb03f0eb9
nymkappa <vialjoris@hotmail.com>
2022-01-21 21:50:57 +0900

    Polish mining page UI
    Make sure to wait for all mining pools queries before continuing

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -532,9 +532,12 @@ class Routes {
     }
   }
 
-  public async getPools(req: Request, res: Response) {
+  public async $getPools(req: Request, res: Response) {
     try {
       let stats = await miningStats.$getPoolsStats(req.query.interval as string);
+      // res.header('Pragma', 'public');
+      // res.header('Cache-control', 'public');
+      // res.setHeader('Expires', new Date(Date.now() + 1000 * 300).toUTCString());
       res.json(stats);
     } catch (e) {
       res.status(500).send(e instanceof Error ? e.message : e);
