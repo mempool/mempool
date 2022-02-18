38b37a3ee7d9bb4227bf70aca85cd8af62e23fd9
nymkappa <vialjoris@hotmail.com>
2022-02-18 15:26:15 +0900

    Re-define sub mining routes properly and use router outlet

---
+++ frontend/src/app/components/hashrate-chart/hashrate-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hashrate-chart',
+  templateUrl: './hashrate-chart.component.html',
+  styleUrls: ['./hashrate-chart.component.scss']
+})
+export class HashrateChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
