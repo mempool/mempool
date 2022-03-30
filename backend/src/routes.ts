12ec6bbf6759a22fc9e45747ce8d3934297c91c4
nymkappa <vialjoris@hotmail.com>
2022-03-30 12:10:20 +0900

    `/api/v1/mining/difficulty/{interval}` is not used

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -576,18 +576,6 @@ class Routes {
     }
   }
 
-  public async $getHistoricalDifficulty(req: Request, res: Response) {
-    try {
-      const stats = await BlocksRepository.$getBlocksDifficulty(req.params.interval ?? null);
-      res.header('Pragma', 'public');
-      res.header('Cache-control', 'public');
-      res.setHeader('Expires', new Date(Date.now() + 1000 * 300).toUTCString());
-      res.json(stats);
-    } catch (e) {
-      res.status(500).send(e instanceof Error ? e.message : e);
-    }
-  }
-
   public async $getPoolsHistoricalHashrate(req: Request, res: Response) {
     try {
       const hashrates = await HashratesRepository.$getPoolsWeeklyHashrate(req.params.interval ?? null);
