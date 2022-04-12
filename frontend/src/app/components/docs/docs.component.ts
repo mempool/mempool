72227236f915bbe49fd1083c3eb5523b486b8811
hunicus <93150691+hunicus@users.noreply.github.com>
2022-04-12 18:06:32 -0400

    Add websocket service to avoid issues with blocks

--- frontend/src/app/components/docs/docs.component.ts
+++ frontend/src/app/components/docs/docs.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Env, StateService } from 'src/app/services/state.service';
+import { WebsocketService } from 'src/app/services/websocket.service';
 
 @Component({
   selector: 'app-docs',
@@ -19,9 +20,11 @@ export class DocsComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private stateService: StateService,
+    private websocket: WebsocketService,
   ) { }
 
   ngOnInit(): void {
+    this.websocket.want(['blocks']);
     const url = this.route.snapshot.url;
     if( url[1].path === "faq" ) {
         this.activeTab = 0;
@@ -34,7 +37,7 @@ export class DocsComponent implements OnInit {
     this.env = this.stateService.env;
     this.showWebSocketTab = ( ! ( ( this.stateService.network === "bisq" ) || ( this.stateService.network === "liquidtestnet" ) ) );
     this.showFaqTab = ( this.env.BASE_MODULE === 'mempool' ) ? true : false;
-    
+
     document.querySelector<HTMLElement>( "html" ).style.scrollBehavior = "smooth";
   }
 
