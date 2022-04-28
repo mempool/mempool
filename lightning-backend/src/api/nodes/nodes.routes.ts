1c3570113ec7e2074b931eb32616e507c64dffb1
softsimon <softsimon@protonmail.com>
2022-04-29 03:57:27 +0400

    Node and channel API

--- lightning-backend/src/api/nodes/nodes.routes.ts
+++ lightning-backend/src/api/nodes/nodes.routes.ts
@@ -1,12 +1,27 @@
 import config from '../../config';
 import { Express, Request, Response } from 'express';
 import nodesApi from './nodes.api';
+import channelsApi from './channels.api';
 class NodesRoutes {
   constructor(app: Express) {
     app
       .get(config.MEMPOOL.API_URL_PREFIX + 'statistics/latest', this.$getGeneralStats)
       .get(config.MEMPOOL.API_URL_PREFIX + 'nodes/top', this.$getTopNodes)
+      .get(config.MEMPOOL.API_URL_PREFIX + 'nodes/:public_key', this.$getNode)
     ;
+  }
+
+  private async $getNode(req: Request, res: Response) {
+    try {
+      const node = await nodesApi.$getNode(req.params.public_key);
+      if (!node) {
+        res.status(404).send('Node not found');
+        return;
+      }
+      res.json(node);
+    } catch (e) {
+      res.status(500).send(e instanceof Error ? e.message : e);
+    }
   }
 
   private async $getGeneralStats(req: Request, res: Response) {
