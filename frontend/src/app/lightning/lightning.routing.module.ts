31d280f7298dc9366b0c2b7f09f9a693241934a0
softsimon <softsimon@protonmail.com>
2022-05-06 16:31:25 +0400

    Adding Lightning wrapper component

--- frontend/src/app/lightning/lightning.routing.module.ts
+++ frontend/src/app/lightning/lightning.routing.module.ts
@@ -1,21 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LightningDashboardComponent } from './lightning-dashboard/lightning-dashboard.component';
+import { LightningWrapperComponent } from './lightning-wrapper/lightning-wrapper.component';
 import { NodeComponent } from './node/node.component';
 import { ChannelComponent } from './channel/channel.component';
 
 const routes: Routes = [
     {
       path: '',
-      component: LightningDashboardComponent,
-    },
-    {
-      path: 'node/:public_key',
-      component: NodeComponent,
-    },
-    {
-      path: 'channel/:short_id',
-      component: ChannelComponent,
+      component: LightningWrapperComponent,
+      children: [
+        {
+          path: '',
+          component: LightningDashboardComponent,
+        },
+        {
+          path: 'node/:public_key',
+          component: NodeComponent,
+        },
+        {
+          path: 'channel/:short_id',
+          component: ChannelComponent,
+        },
+        {
+          path: '**',
+          redirectTo: ''
+        }
+      ]
     },
     {
       path: '**',
