d4c9f6decb95aa9fb02cc0e353064214d70b5fc5
Mononaut <github@monospace.live>
2022-05-30 21:53:39 +0000

    Implement WebGL projected block visualization

---
+++ frontend/src/app/components/mempool-block-overview/block-scene.ts
@@ -0,0 +1,407 @@
+import TxSprite from './tx-sprite'
+import TxView from './tx-view'
+import { Square } from './sprite-types'
+
+export default class BlockScene {
+  scene: { count: number, offset: { x: number, y: number}};
+  txs: { [key: string]: TxView };
+  width: number;
+  height: number;
+  gridWidth: number;
+  gridHeight: number;
+  gridSize: number;
+  vbytesPerUnit: number;
+  unitPadding: number;
+  unitWidth: number;
+  initialised: boolean;
+  layout: BlockLayout;
+  dirty: boolean;
+
+  constructor ({ width, height, resolution, blockLimit }: { width: number, height: number, resolution: number, blockLimit: number}) {
+    this.init({ width, height, resolution, blockLimit })
+  }
+
+  destroy (): void {
+    Object.values(this.txs).forEach(tx => tx.destroy())
+  }
+
+  resize ({ width = this.width, height = this.height }: { width?: number, height?: number}): void {
+    this.width = width
+    this.height = height
+    this.gridSize = this.width / this.gridWidth
+    this.unitPadding =  Math.floor(Math.max(1, width / 1000))
+    this.unitWidth = this.gridSize - (this.unitPadding * 2)
+
+    this.dirty = true
+  }
+
+  // Animate new block entering scene
+  enter (txs: TxView[], direction) {
+    this.replace(txs, [], direction)
+  }
+
+  // Animate block leaving scene
+  exit (direction: string): TxView[] {
+    const removed = []
+    const startTime = performance.now()
+    Object.values(this.txs).forEach(tx => {
+      this.remove(tx.txid, startTime, direction)
+      removed.push(tx)
+    })
+    return removed
+  }
+
+  // Reset layout and replace with new set of transactions
+  replace (txs: TxView[], remove: TxView[], direction: string = 'left'): void {
+    const startTime = performance.now()
+    this.removeBatch(remove.map(tx => tx.txid), startTime, direction)
+
+    // clean up sprites
+    setTimeout(() => {
+      remove.forEach(tx => {
+        tx.destroy()
+      })
+    }, 1000)
+
+    this.layout = new BlockLayout({ width: this.gridWidth, height: this.gridHeight })
+
+    txs.sort((a,b) => { return b.feerate - a.feerate }).forEach(tx => {
+      this.insert(tx, startTime, direction)
+    })
+  }
+
+  private init ({ width, height, resolution, blockLimit }: { width: number, height: number, resolution: number, blockLimit: number}): void {
+    this.scene = {
+      count: 0,
+      offset: {
+        x: 0,
+        y: 0
+      }
+    }
+
+    // Set the scale of the visualization (with a 5% margin)
+    this.vbytesPerUnit = blockLimit / Math.pow(resolution / 1.05, 2)
+    this.gridWidth = resolution
+    this.gridHeight = resolution
+    this.resize({ width, height })
+    this.layout = new BlockLayout({ width: this.gridWidth, height: this.gridHeight })
+
+    this.txs = {}
+
+    this.initialised = true
+    this.dirty = true
+  }
+
+  private insert (tx: TxView, startTime: number, direction: string = 'left'): void {
+    this.txs[tx.txid] = tx
+    this.place(tx)
+    this.updateTx(tx, startTime, direction)
+  }
+
+  private updateTx (tx: TxView, startTime: number, direction: string = 'left'): void {
+    if (tx.dirty || this.dirty) {
+      this.saveGridToScreenPosition(tx)
+      this.setTxOnScreen(tx, startTime, direction)
+    }
+  }
+
+  private setTxOnScreen (tx: TxView, startTime: number, direction: string = 'left'): void {
+    if (!tx.initialised) {
+      const txColor = tx.getColor()
+      tx.update({
+        display: {
+          position: {
+            x: tx.screenPosition.x + (direction == 'right' ? -this.width : this.width) * 1.4,
+            y: tx.screenPosition.y,
+            s: tx.screenPosition.s
+          },
+          color: txColor,
+        },
+        start: startTime,
+        delay: 0,
+      })
+      tx.update({
+        display: {
+          position: tx.screenPosition,
+          color: txColor
+        },
+        duration: 1000,
+        start: startTime,
+        delay: 50,
+      })
+    } else {
+      tx.update({
+        display: {
+          position: tx.screenPosition
+        },
+        duration: 1000,
+        minDuration: 1000,
+        start: startTime,
+        delay: 50,
+      })
+    }
+  }
+
+  private updateAll (startTime: number, direction: string = 'left'): void {
+    this.scene.count = 0
+    const ids = this.getTxList()
+    startTime = startTime || performance.now()
+    for (let i = 0; i < ids.length; i++) {
+      this.updateTx(this.txs[ids[i]], startTime, direction)
+    }
+    this.dirty = false
+  }
+
+  private remove (id: string, startTime: number, direction: string = 'left'): TxView | void {
+    const tx = this.txs[id]
+    if (tx) {
+      this.layout.remove(tx)
+      tx.update({
+        display: {
+          position: {
+            x: tx.screenPosition.x + (direction == 'right' ? this.width : -this.width) * 1.4,
+            y: this.txs[id].screenPosition.y,
+          }
+        },
+        duration: 1000,
+        start: startTime,
+        delay: 50
+      })
+    }
+    delete this.txs[id]
+    return tx
+  }
+
+  private getTxList (): string[] {
+    return Object.keys(this.txs)
+  }
+
+  private saveGridToScreenPosition (tx: TxView): void {
+    tx.screenPosition = this.gridToScreen(tx.gridPosition)
+  }
+
+  // convert grid coordinates to screen coordinates
+  private gridToScreen (position: Square | void): Square {
+    if (position) {
+      const slotSize = (position.s * this.gridSize)
+      const squareSize = slotSize - (this.unitPadding * 2)
+
+      // The grid is laid out notionally left-to-right, bottom-to-top
+      // So we rotate 90deg counterclockwise then flip the y axis
+      //
+      //    grid                             screen
+      //  ________          ________        ________
+      // |        |        |       b|      |       a|
+      // |        | rotate |        | flip |     c  |
+      // |  c     |   -->  |      c |  --> |        |
+      // |a______b|        |_______a|      |_______b|
+      return {
+        x: this.width + (this.unitPadding * 2) - (this.gridSize * position.y) - slotSize,
+        y: this.height - ((this.gridSize * position.x) + (slotSize - this.unitPadding)),
+        s: squareSize
+      }
+    } else {
+      return { x: 0, y: 0, s: 0 }
+    }
+  }
+
+  // calculates and returns the size of the tx in multiples of the grid size
+  private txSize (tx: TxView): number {
+    let scale = Math.max(1,Math.round(Math.sqrt(tx.vsize / this.vbytesPerUnit)))
+    return Math.min(this.gridWidth, Math.max(1, scale)) // bound between 1 and the max displayable size (just in case!)
+  }
+
+  private place (tx: TxView): void {
+    const size = this.txSize(tx)
+    this.layout.insert(tx, size)
+  }
+
+  private removeBatch (ids: string[], startTime: number, direction: string = 'left'): (TxView | void)[] {
+    if (!startTime) startTime = performance.now()
+    return ids.map(id => {
+      return this.remove(id, startTime, direction)
+    }).filter(tx => !!tx)
+  }
+}
+
+
+class Slot {
+  l: number
+  r: number
+  w: number
+
+  constructor (l: number, r: number) {
+    this.l = l
+    this.r = r
+    this.w = r - l
+  }
+
+  intersects (slot: Slot): boolean {
+    return !((slot.r <= this.l) || (slot.l >= this.r))
+  }
+
+  subtract (slot: Slot): Slot[] | void {
+    if (this.intersects(slot)) {
+      // from middle
+      if (slot.l > this.l && slot.r < this.r) {
+        return [
+          new Slot(this.l, slot.l),
+          new Slot(slot.r, this.r)
+        ]
+      } // totally covered
+      else if (slot.l <= this.l && slot.r >= this.r) {
+        return []
+      } // from left side
+      else if (slot.l <= this.l) {
+        if (slot.r == this.r) return []
+        else return [new Slot(slot.r, this.r)]
+      } // from right side
+      else if (slot.r >= this.r) {
+        if (slot.l == this.l) return []
+        else return [new Slot(this.l, slot.l)]
+      }
+    } else return [this]
+  }
+}
+
+class TxSlot extends Slot {
+  tx: TxView
+
+  constructor (l: number, r: number, tx: TxView) {
+    super(l, r)
+    this.tx = tx
+  }
+}
+
+class Row {
+  y: number
+  w: number
+  filled: TxSlot[]
+  slots: Slot[]
+
+
+  constructor (y: number, width: number) {
+    this.y = y
+    this.w = width
+    this.filled = []
+    this.slots = [new Slot(0, this.w)]
+  }
+
+  // insert a transaction w/ given width into row starting at position x
+  insert (x: number, w: number, tx: TxView): void {
+    const newSlot = new TxSlot(x, x + w, tx)
+    // insert into filled list
+    let index = this.filled.findIndex((slot) => { return slot.l >= newSlot.r })
+    if (index < 0) index = this.filled.length
+    this.filled.splice(index || 0, 0, newSlot)
+    // subtract from overlapping slots
+    for (let i = 0; i < this.slots.length; i++) {
+      if (newSlot.intersects(this.slots[i])) {
+        const diff = this.slots[i].subtract(newSlot)
+        if (diff) {
+          this.slots.splice(i, 1, ...diff)
+          i += diff.length - 1
+        }
+      }
+    }
+  }
+
+  remove (x: number, w: number): void {
+    const txIndex = this.filled.findIndex((slot) => { return slot.l == x })
+    this.filled.splice(txIndex, 1)
+
+    const newSlot = new Slot(x, x + w)
+    let slotIndex = this.slots.findIndex((slot) => { return slot.l >= newSlot.r })
+    if (slotIndex < 0) slotIndex = this.slots.length
+    this.slots.splice(slotIndex || 0, 0, newSlot)
+    this.normalize()
+  }
+
+  // merge any contiguous empty slots
+  private normalize (): void {
+    for (let i = 0; i < this.slots.length - 1; i++) {
+      if (this.slots[i].r == this.slots[i+1].l) {
+        this.slots[i].r = this.slots[i+1].r
+        this.slots[i].w += this.slots[i+1].w
+        this.slots.splice(i+1, 1)
+        i--
+      }
+    }
+  }
+}
+
+class BlockLayout {
+  width: number;
+  height: number;
+  rows: Row[];
+  txPositions: { [key: string]: Square }
+
+
+  constructor ({ width, height } : { width: number, height: number }) {
+    this.width = width
+    this.height = height
+    this.rows = [new Row(0, this.width)]
+    this.txPositions = {}
+  }
+
+  addRow (): void {
+    this.rows.push(new Row(this.rows.length, this.width))
+  }
+
+  remove (tx: TxView) {
+    const position = this.txPositions[tx.txid]
+    if (position) {
+      for (let y = position.y; y < position.y + position.s && y < this.rows.length; y++) {
+        this.rows[y].remove(position.x, position.s)
+      }
+    }
+    delete this.txPositions[tx.txid]
+  }
+
+  insert (tx: TxView, width: number): Square {
+    const fit = this.fit(tx, width)
+
+    // insert the tx into rows at that position
+    for (let y = fit.y; y < fit.y + width; y++) {
+      if (y >= this.rows.length) this.addRow()
+      this.rows[y].insert(fit.x, width, tx)
+    }
+    const position = { x: fit.x, y: fit.y, s: width }
+    this.txPositions[tx.txid] = position
+    tx.applyGridPosition(position)
+    return position
+  }
+
+  // Find the first slot large enough to hold a transaction of this size
+  fit (tx: TxView, width: number): Square {
+    let fit
+    for (let y = 0; y < this.rows.length && !fit; y++) {
+      fit = this.findFit(0, this.width, y, y, width)
+    }
+    // fall back to placing tx in a new row at the top of the layout
+    if (!fit) {
+      fit = { x: 0, y: this.rows.length }
+    }
+    return fit
+  }
+
+  // recursively check rows to see if there's space for a tx (depth-first)
+  // left/right: initial column boundaries to check
+  // row: current row to check
+  // start: starting row
+  // size: size of space needed
+  findFit (left: number, right: number, row: number, start: number, size: number) : Square {
+    if ((row - start) >= size || row >= this.rows.length) {
+      return { x: left, y: start }
+    }
+    for (let i = 0; i < this.rows[row].slots.length; i++) {
+      const slot = this.rows[row].slots[i]
+      const l = Math.max(left, slot.l)
+      const r = Math.min(right, slot.r)
+      if (r - l >= size) {
+        const fit = this.findFit(l, r, row + 1, start, size)
+        if (fit) return fit
+      }
+    }
+  }
+}
--- frontend/src/app/components/mempool-block-overview/mempool-block-overview.component.ts
+++ frontend/src/app/components/mempool-block-overview/mempool-block-overview.component.ts
@@ -1,17 +1,36 @@
-import { Component, Input, OnInit, OnDestroy,  OnChanges, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ElementRef, ViewChild, HostListener, Input, OnInit, OnDestroy,  OnChanges, ChangeDetectionStrategy, NgZone } from '@angular/core';
 import { StateService } from 'src/app/services/state.service';
 import { MempoolBlockWithTransactions } from 'src/app/interfaces/websocket.interface';
 import { Observable, Subscription } from 'rxjs';
 import { WebsocketService } from 'src/app/services/websocket.service';
+import { FastVertexArray } from './fast-vertex-array';
+import BlockScene from './block-scene';
+import TxSprite from './tx-sprite';
+import TxView from './tx-view';
 
 @Component({
   selector: 'app-mempool-block-overview',
   templateUrl: './mempool-block-overview.component.html',
-  styleUrls: [],
+  styleUrls: ['./mempool-block-overview.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MempoolBlockOverviewComponent implements OnInit, OnDestroy, OnChanges {
   @Input() index: number;
+
+  @ViewChild('blockCanvas')
+  canvas: ElementRef<HTMLCanvasElement>;
+
+  gl: WebGLRenderingContext;
+  animationFrameRequest: number;
+  displayWidth: number;
+  displayHeight: number;
+  shaderProgram: WebGLProgram;
+  vertexArray: FastVertexArray;
+  running: boolean;
+  scene: BlockScene;
+  txViews: { [key: string]: TxView };
+  lastBlockHeight: number;
+  blockIndex: number;
 
   sub: Subscription;
   mempoolBlock$: Observable<MempoolBlockWithTransactions>;
@@ -19,7 +38,11 @@ export class MempoolBlockOverviewComponent implements OnInit, OnDestroy, OnChang
   constructor(
     public stateService: StateService,
     private websocketService: WebsocketService,
-  ) { }
+    readonly _ngZone: NgZone,
+  ) {
+    this.vertexArray = new FastVertexArray(512, TxSprite.dataSize)
+    this.txViews = {}
+  }
 
   ngOnInit(): void {
     this.websocketService.startTrackMempoolBlock(this.index);
@@ -29,8 +52,18 @@ export class MempoolBlockOverviewComponent implements OnInit, OnDestroy, OnChang
     })
   }
 
+  ngAfterViewInit(): void {
+    this.canvas.nativeElement.addEventListener("webglcontextlost", this.handleContextLost, false)
+    this.canvas.nativeElement.addEventListener("webglcontextrestored", this.handleContextRestored, false)
+    this.gl = this.canvas.nativeElement.getContext('webgl')
+    this.initCanvas()
+
+    this.resizeCanvas()
+  }
+
   ngOnChanges(changes): void {
     if (changes.index) {
+      this.clearBlock(changes.index.currentValue)
       this.websocketService.startTrackMempoolBlock(changes.index.currentValue);
     }
   }
@@ -40,7 +73,271 @@ export class MempoolBlockOverviewComponent implements OnInit, OnDestroy, OnChang
     this.websocketService.stopTrackMempoolBlock();
   }
 
+  clearBlock(index: number): void {
+    if (this.scene && index != this.blockIndex) {
+      const direction = (this.blockIndex == null || this.index < this.blockIndex) ? 'left' : 'right'
+      const removed = this.scene.exit(direction)
+      setTimeout(() => {
+        removed.forEach(tx => tx.destroy())
+      }, 1000)
+      this.txViews = {}
+      this.scene = null
+    }
+  }
+
   updateBlock(block: MempoolBlockWithTransactions): void {
-    
+    if (!this.scene) {
+      this.scene = new BlockScene({ width: this.displayWidth, height: this.displayHeight, resolution: 75, blockLimit: this.stateService.blockVSize })
+    }
+    const blockMined = (this.stateService.latestBlockHeight > this.lastBlockHeight)
+
+    const nextIds = {}
+    let remove = []
+    let add = []
+    block.transactions.forEach(tx => {
+      nextIds[tx.txid] = true
+    })
+
+    // List old transactions to remove
+    Object.keys(this.txViews).forEach(txid => {
+      if (!nextIds[txid]) {
+        remove.push(this.txViews[txid])
+        delete this.txViews[txid]
+      }
+    })
+
+    // List new transactions to add
+    block.transactions.forEach(tx => {
+      if (!this.txViews[tx.txid]) {
+        const txView = new TxView(tx, this.vertexArray)
+        this.txViews[tx.txid] = txView
+        add.push(txView)
+      }
+    })
+
+    if (this.blockIndex != this.index) {
+      const direction = (this.blockIndex == null || this.index < this.blockIndex) ? 'left' : 'right'
+      this.scene.enter(Object.values(this.txViews), direction)
+    } else if (blockMined) {
+      this.scene.replace(Object.values(this.txViews), remove, 'right')
+    } else {
+      this.scene.replace(Object.values(this.txViews), remove, 'left')
+    }
+
+    this.lastBlockHeight = this.stateService.latestBlockHeight
+    this.blockIndex = this.index
+  }
+
+  initCanvas (): void {
+    this.gl.clearColor(0.0, 0.0, 0.0, 0.0)
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT)
+
+    const shaderSet = [
+      {
+        type: this.gl.VERTEX_SHADER,
+        src: vertShaderSrc
+      },
+      {
+        type: this.gl.FRAGMENT_SHADER,
+        src: fragShaderSrc
+      }
+    ]
+
+    this.shaderProgram = this.buildShaderProgram(shaderSet)
+
+    this.gl.useProgram(this.shaderProgram)
+
+    // Set up alpha blending
+    this.gl.enable(this.gl.BLEND);
+    this.gl.blendFunc(this.gl.ONE, this.gl.ONE_MINUS_SRC_ALPHA);
+
+    const glBuffer = this.gl.createBuffer()
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, glBuffer)
+
+    /* SET UP SHADER ATTRIBUTES */
+    Object.keys(attribs).forEach((key, i) => {
+      attribs[key].pointer = this.gl.getAttribLocation(this.shaderProgram, key)
+      this.gl.enableVertexAttribArray(attribs[key].pointer);
+    })
+
+    this.start()
+  }
+
+  handleContextLost(event): void {
+    event.preventDefault()
+    cancelAnimationFrame(this.animationFrameRequest)
+    this.animationFrameRequest = null
+    this.running = false
+  }
+
+  handleContextRestored(event): void {
+    this.initCanvas()
+  }
+
+  @HostListener('window:resize', ['$event'])
+  resizeCanvas(): void {
+    this.displayWidth = this.canvas.nativeElement.parentElement.clientWidth
+    this.displayHeight = this.canvas.nativeElement.parentElement.clientHeight
+    this.canvas.nativeElement.width = this.displayWidth
+    this.canvas.nativeElement.height = this.displayHeight
+    if (this.gl) this.gl.viewport(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height)
+    if (this.scene) this.scene.resize({ width: this.displayWidth, height: this.displayHeight })
+  }
+
+  compileShader(src, type): WebGLShader {
+    let shader = this.gl.createShader(type)
+
+    this.gl.shaderSource(shader, src)
+    this.gl.compileShader(shader)
+
+    if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
+      console.log(`Error compiling ${type === this.gl.VERTEX_SHADER ? "vertex" : "fragment"} shader:`)
+      console.log(this.gl.getShaderInfoLog(shader))
+    }
+    return shader
+  }
+
+  buildShaderProgram(shaderInfo): WebGLProgram {
+    let program = this.gl.createProgram()
+
+    shaderInfo.forEach((desc) => {
+      let shader = this.compileShader(desc.src, desc.type)
+      if (shader) {
+        this.gl.attachShader(program, shader)
+      }
+    })
+
+    this.gl.linkProgram(program)
+
+    if (!this.gl.getProgramParameter(program, this.gl.LINK_STATUS)) {
+      console.log("Error linking shader program:")
+      console.log(this.gl.getProgramInfoLog(program))
+    }
+
+    return program
+  }
+
+  start(): void {
+    this.running = true
+    this._ngZone.runOutsideAngular(() => this.run())
+  }
+
+  run (now?: DOMHighResTimeStamp): void {
+    if (!now) {
+      now = performance.now()
+    }
+
+    /* SET UP SHADER UNIFORMS */
+    // screen dimensions
+    this.gl.uniform2f(this.gl.getUniformLocation(this.shaderProgram, 'screenSize'), this.displayWidth, this.displayHeight)
+    // frame timestamp
+    this.gl.uniform1f(this.gl.getUniformLocation(this.shaderProgram, 'now'), now)
+
+    /* SET UP SHADER ATTRIBUTES */
+    Object.keys(attribs).forEach((key, i) => {
+      this.gl.vertexAttribPointer(attribs[key].pointer,
+      attribs[key].count,  // number of primitives in this attribute
+      this.gl[attribs[key].type],  // type of primitive in this attribute (e.g. gl.FLOAT)
+      false, // never normalised
+      stride,   // distance between values of the same attribute
+      attribs[key].offset);  // offset of the first value
+    })
+
+    const pointArray = this.vertexArray.getVertexData()
+
+    if (pointArray.length) {
+      this.gl.bufferData(this.gl.ARRAY_BUFFER, pointArray, this.gl.DYNAMIC_DRAW)
+      this.gl.drawArrays(this.gl.TRIANGLES, 0, pointArray.length / TxSprite.vertexSize)
+    }
+
+    /* LOOP */
+    if (this.running) {
+      if (this.animationFrameRequest) {
+        cancelAnimationFrame(this.animationFrameRequest)
+        this.animationFrameRequest = null
+      }
+      this.animationFrameRequest = requestAnimationFrame(() => this.run());
+    }
   }
 }
+
+// WebGL shader attributes
+const attribs = {
+  offset: { type: 'FLOAT', count: 2, pointer: null, offset: 0 },
+  posX: { type: 'FLOAT', count: 4, pointer: null, offset: 0 },
+  posY: { type: 'FLOAT', count: 4, pointer: null, offset: 0 },
+  posR: { type: 'FLOAT', count: 4, pointer: null, offset: 0 },
+  colR: { type: 'FLOAT', count: 4, pointer: null, offset: 0 },
+  colG: { type: 'FLOAT', count: 4, pointer: null, offset: 0 },
+  colB: { type: 'FLOAT', count: 4, pointer: null, offset: 0 },
+  colA: { type: 'FLOAT', count: 4, pointer: null, offset: 0 }
+}
+// Calculate the number of bytes per vertex based on specified attributes
+const stride = Object.values(attribs).reduce((total, attrib) => {
+  return total + (attrib.count * 4)
+}, 0)
+// Calculate vertex attribute offsets
+for (let i = 0, offset = 0; i < Object.keys(attribs).length; i++) {
+  let attrib = Object.values(attribs)[i]
+  attrib.offset = offset
+  offset += (attrib.count * 4)
+}
+
+const vertShaderSrc = `
+varying lowp vec4 vColor;
+
+// each attribute contains [x: startValue, y: endValue, z: startTime, w: rate]
+// shader interpolates between start and end values at the given rate, from the given time
+
+attribute vec2 offset;
+attribute vec4 posX;
+attribute vec4 posY;
+attribute vec4 posR;
+attribute vec4 colR;
+attribute vec4 colG;
+attribute vec4 colB;
+attribute vec4 colA;
+
+uniform vec2 screenSize;
+uniform float now;
+
+float smootherstep(float x) {
+  x = clamp(x, 0.0, 1.0);
+  float ix = 1.0 - x;
+  x = x * x;
+  return x / (x + ix * ix);
+}
+
+float interpolateAttribute(vec4 attr) {
+  float d = (now - attr.z) * attr.w;
+  float delta = smootherstep(d);
+  return mix(attr.x, attr.y, delta);
+}
+
+void main() {
+  vec4 screenTransform = vec4(2.0 / screenSize.x, 2.0 / screenSize.y, -1.0, -1.0);
+  // vec4 screenTransform = vec4(1.0 / screenSize.x, 1.0 / screenSize.y, -0.5, -0.5);
+
+  float radius = interpolateAttribute(posR);
+  vec2 position = vec2(interpolateAttribute(posX), interpolateAttribute(posY)) + (radius * offset);
+
+  gl_Position = vec4(position * screenTransform.xy + screenTransform.zw, 1.0, 1.0);
+
+  float red = interpolateAttribute(colR);
+  float green = interpolateAttribute(colG);
+  float blue = interpolateAttribute(colB);
+  float alpha = interpolateAttribute(colA);
+
+  vColor = vec4(red, green, blue, alpha);
+}
+`
+
+const fragShaderSrc = `
+varying lowp vec4 vColor;
+
+void main() {
+  gl_FragColor = vColor;
+  // premultiply alpha
+  gl_FragColor.rgb *= gl_FragColor.a;
+}
+`
