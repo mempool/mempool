e7a109d7994055fee432be58f83255f6807f9d80
Felipe Knorr Kuhn <100320+knorrium@users.noreply.github.com>
2022-07-21 21:41:33 -0700

    Fix tests trying to click on the TV button on the navbar

--- frontend/cypress/e2e/signet/signet.spec.ts
+++ frontend/cypress/e2e/signet/signet.spec.ts
@@ -63,7 +63,7 @@ describe('Signet', () => {
     describe('tv mode', () => {
       it('loads the tv screen - desktop', () => {
         cy.viewport('macbook-16');
-        cy.visit('/signet');
+        cy.visit('/signet/graphs');
         cy.waitForSkeletonGone();
         cy.get('#btn-tv').click().then(() => {
           cy.get('.chart-holder').should('be.visible');
@@ -73,18 +73,16 @@ describe('Signet', () => {
       });
 
       it('loads the tv screen - mobile', () => {
-        cy.visit('/signet');
+        cy.visit('/signet/graphs');
         cy.waitForSkeletonGone();
         cy.get('#btn-tv').click().then(() => {
           cy.viewport('iphone-8');
           cy.get('.chart-holder').should('be.visible');
           cy.get('.tv-only').should('not.exist');
-          //TODO: Remove comment when the bug is fixed
-          //cy.get('#mempool-block-0').should('be.visible');
+          cy.get('#mempool-block-0').should('be.visible');
         });
       });
     });
-
 
     it('loads the api screen', () => {
       cy.visit('/signet');
