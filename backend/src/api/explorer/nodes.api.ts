f60ef05223ae66629d12d8dd764f1520fb9b438f
nymkappa <vialjoris@hotmail.com>
2022-08-04 13:11:24 +0200

    Re-applied missing fix from https://github.com/mempool/mempool/pull/2233

--- backend/src/api/explorer/nodes.api.ts
+++ backend/src/api/explorer/nodes.api.ts
@@ -70,7 +70,7 @@ class NodesApi {
       const latestDate = rows[0].maxAdded;
 
       const query = `
-        SELECT nodes.public_key, nodes.alias, node_stats.capacity, node_stats.channels
+        SELECT nodes.public_key, IF(nodes.alias = '', SUBSTRING(nodes.public_key, 1, 20), alias) as alias, node_stats.capacity, node_stats.channels
         FROM node_stats
         JOIN nodes ON nodes.public_key = node_stats.public_key
         WHERE added = FROM_UNIXTIME(${latestDate})
@@ -92,7 +92,7 @@ class NodesApi {
       const latestDate = rows[0].maxAdded;
 
       const query = `
-        SELECT nodes.public_key, nodes.alias, node_stats.capacity, node_stats.channels
+        SELECT nodes.public_key, IF(nodes.alias = '', SUBSTRING(nodes.public_key, 1, 20), alias) as alias, node_stats.capacity, node_stats.channels
         FROM node_stats
         JOIN nodes ON nodes.public_key = node_stats.public_key
         WHERE added = FROM_UNIXTIME(${latestDate})
