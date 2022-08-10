239cbf09ca10399a23ff5698af88e6fa14041de9
nymkappa <vialjoris@hotmail.com>
2022-08-10 17:03:11 +0200

    Fix channel rendering issue

--- frontend/src/app/lightning/nodes-channels-map/nodes-channels-map.component.ts
+++ frontend/src/app/lightning/nodes-channels-map/nodes-channels-map.component.ts
@@ -197,7 +197,6 @@ export class NodesChannelsMap implements OnInit, OnDestroy {
       series: [
         {
           large: true,
-          progressive: 200,
           type: 'scatter',
           data: nodes,
           coordinateSystem: 'geo',
@@ -228,7 +227,7 @@ export class NodesChannelsMap implements OnInit, OnDestroy {
           zlevel: 1,
         },
         {
-          large: true,
+          large: false,
           progressive: 200,
           silent: true,
           type: 'lines',
