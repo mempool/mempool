d051538c6a2ba7f49e214a988c1bdea59a9d92a4
softsimon <softsimon@protonmail.com>
2022-08-27 09:54:16 +0200

    Fix for broken SQL to load Lightning nodes on ISP page

--- backend/src/api/explorer/nodes.api.ts
+++ backend/src/api/explorer/nodes.api.ts
@@ -412,6 +412,7 @@ class NodesApi {
           nodes.alias, UNIX_TIMESTAMP(nodes.first_seen) as first_seen, UNIX_TIMESTAMP(nodes.updated_at) as updated_at,
           geo_names_city.names as city, geo_names_country.names as country,
           geo_names_iso.names as iso_code, geo_names_subdivision.names as subdivision
+        FROM nodes
         LEFT JOIN geo_names geo_names_country ON geo_names_country.id = nodes.country_id AND geo_names_country.type = 'country'
         LEFT JOIN geo_names geo_names_city ON geo_names_city.id = nodes.city_id AND geo_names_city.type = 'city'
         LEFT JOIN geo_names geo_names_iso ON geo_names_iso.id = nodes.country_id AND geo_names_iso.type = 'country_iso_code'
