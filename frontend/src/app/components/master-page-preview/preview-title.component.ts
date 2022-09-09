4fb77a9a458c46b0b8b06ad19d44bacf91bd23c9
Mononaut <github@monospace.live>
2022-09-09 18:14:16 +0000

    Add network to preview headers & inc font size

---
+++ frontend/src/app/components/master-page-preview/preview-title.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { StateService } from '../../services/state.service';
+import { Observable, merge, of } from 'rxjs';
+
+@Component({
+  selector: 'app-preview-title',
+  templateUrl: './preview-title.component.html',
+  styleUrls: [],
+})
+export class PreviewTitleComponent implements OnInit {
+  network$: Observable<string>;
+
+  constructor(
+    public stateService: StateService,
+  ) { }
+
+  ngOnInit() {
+    this.network$ = merge(of(''), this.stateService.networkChanged$);
+  }
+}
