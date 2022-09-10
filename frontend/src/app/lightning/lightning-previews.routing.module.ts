aa3e7e44f39e4b750c265ef51ffea25b21095e90
Mononaut <github@monospace.live>
2022-09-10 14:53:52 +0000

    Add lighting ISP preview

--- frontend/src/app/lightning/lightning-previews.routing.module.ts
+++ frontend/src/app/lightning/lightning-previews.routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NodePreviewComponent } from './node/node-preview.component';
 import { ChannelPreviewComponent } from './channel/channel-preview.component';
+import { NodesPerISPPreview } from './nodes-per-isp/nodes-per-isp-preview.component';
 
 const routes: Routes = [
     {
@@ -11,6 +12,10 @@ const routes: Routes = [
     {
       path: 'channel/:short_id',
       component: ChannelPreviewComponent,
+    },
+    {
+      path: 'nodes/isp/:isp',
+      component: NodesPerISPPreview,
     },
     {
       path: '**',
