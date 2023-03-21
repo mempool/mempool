cb8fdb5e8de98f3bede276fa18ed4d6874266c38
wiz <j@wiz.biz>
2023-03-21 15:56:11 +0900

    Hack docker frontend entrypoint to auto-enable lightning

--- docker/frontend/entrypoint.sh
+++ docker/frontend/entrypoint.sh
@@ -10,6 +10,8 @@ cp /etc/nginx/nginx.conf /patch/nginx.conf
 sed -i "s/__MEMPOOL_FRONTEND_HTTP_PORT__/${__MEMPOOL_FRONTEND_HTTP_PORT__}/g" /patch/nginx.conf
 cat /patch/nginx.conf > /etc/nginx/nginx.conf
 
+[ "${APP_LIGHTNING_NODE_PORT}" = "9735" ] && LIGHTNING=true
+
 # Runtime overrides - read env vars defined in docker compose
 
 __TESTNET_ENABLED__=${TESTNET_ENABLED:=false}
