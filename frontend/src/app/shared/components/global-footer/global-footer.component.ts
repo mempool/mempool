f63613c7c309de76afeff7f8bcc22faebe071610
softsimon <softsimon@protonmail.com>
2023-05-05 01:38:58 +0400

    Global footer component that fixes Liquid

---
+++ frontend/src/app/shared/components/global-footer/global-footer.component.ts
@@ -0,0 +1,28 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { NavigationService } from '../../../services/navigation.service';
+import { Env, StateService } from '../../../services/state.service';
+
+@Component({
+  selector: 'app-global-footer',
+  templateUrl: './global-footer.component.html',
+  styleUrls: ['./global-footer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class GlobalFooterComponent implements OnInit {
+  env: Env;
+  networkPaths: { [network: string]: string };
+  officialMempoolSpace = this.stateService.env.OFFICIAL_MEMPOOL_SPACE;
+
+  constructor(
+    public stateService: StateService,
+    private navigationService: NavigationService,
+  ) {}
+
+  ngOnInit(): void {
+    this.env = this.stateService.env;
+    this.navigationService.subnetPaths.subscribe((paths) => {
+      this.networkPaths = paths;
+    });
+  }
+
+}
