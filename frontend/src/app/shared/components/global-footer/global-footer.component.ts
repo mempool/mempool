fc6b853a722a0df1512c7f33e060ddafc59e18d2
hunicus <93150691+hunicus@users.noreply.github.com>
2023-05-06 04:10:17 -0400

    Add initial content to big footer

--- frontend/src/app/shared/components/global-footer/global-footer.component.ts
+++ frontend/src/app/shared/components/global-footer/global-footer.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { NavigationService } from '../../../services/navigation.service';
 import { Env, StateService } from '../../../services/state.service';
+import { IBackendInfo } from '../../../interfaces/websocket.interface';
 
 @Component({
   selector: 'app-global-footer',
@@ -14,6 +15,9 @@ export class GlobalFooterComponent implements OnInit {
   networkPaths: { [network: string]: string };
   officialMempoolSpace = this.stateService.env.OFFICIAL_MEMPOOL_SPACE;
   networkPaths$: Observable<Record<string, string>>;
+  backendInfo$: Observable<IBackendInfo>;
+  frontendGitCommitHash = this.stateService.env.GIT_COMMIT_HASH;
+  packetJsonVersion = this.stateService.env.PACKAGE_JSON_VERSION;
 
   constructor(
     public stateService: StateService,
@@ -23,6 +27,7 @@ export class GlobalFooterComponent implements OnInit {
   ngOnInit(): void {
     this.env = this.stateService.env;
     this.networkPaths$ = this.navigationService.subnetPaths;
+    this.backendInfo$ = this.stateService.backendInfo$;
   }
 
 }
