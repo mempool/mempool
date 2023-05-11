8873560df9d1a3cb9a98d2bf068a15aa957eb2ac
nymkappa <1612910616@pm.me>
2023-05-11 18:00:52 +0200

    [footer] dynamic CTA button based on login status

--- frontend/src/app/shared/components/global-footer/global-footer.component.ts
+++ frontend/src/app/shared/components/global-footer/global-footer.component.ts
@@ -5,6 +5,7 @@ import { Env, StateService } from '../../../services/state.service';
 import { IBackendInfo } from '../../../interfaces/websocket.interface';
 import { LanguageService } from '../../../services/language.service';
 import { NavigationService } from '../../../services/navigation.service';
+import { StorageService } from '../../../services/storage.service';
 
 @Component({
   selector: 'app-global-footer',
@@ -23,12 +24,15 @@ export class GlobalFooterComponent implements OnInit {
   network$: Observable<string>;
   networkPaths: { [network: string]: string };
   currentNetwork = '';
+  loggedIn = false;
+  username = null;
 
   constructor(
     public stateService: StateService,
     private languageService: LanguageService,
     private navigationService: NavigationService,
     @Inject(LOCALE_ID) public locale: string,
+    private storageService: StorageService
   ) {}
 
   ngOnInit(): void {
@@ -46,6 +50,14 @@ export class GlobalFooterComponent implements OnInit {
     this.network$.pipe(takeUntil(this.destroy$)).subscribe((network) => {
       this.currentNetwork = network;
     });
+
+    this.loggedIn = this.storageService.getValue('auth') !== null;
+    const auth = JSON.parse(this.storageService.getValue('auth'));
+    if (auth?.user?.username) {
+      this.username = auth.user.username;
+    } else {
+      this.username = null;
+    }
   }
 
   ngOnDestroy(): void {
