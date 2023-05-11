107746feecb473c6d99a1f853f4dc3ada98a0f01
softsimon <softsimon@protonmail.com>
2023-05-11 11:38:57 -0500

    Global footer fixes

--- frontend/src/app/shared/components/global-footer/global-footer.component.ts
+++ frontend/src/app/shared/components/global-footer/global-footer.component.ts
@@ -1,6 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { NavigationService } from '../../../services/navigation.service';
 import { Env, StateService } from '../../../services/state.service';
 import { IBackendInfo } from '../../../interfaces/websocket.interface';
 
@@ -14,19 +13,16 @@ export class GlobalFooterComponent implements OnInit {
   env: Env;
   networkPaths: { [network: string]: string };
   officialMempoolSpace = this.stateService.env.OFFICIAL_MEMPOOL_SPACE;
-  networkPaths$: Observable<Record<string, string>>;
   backendInfo$: Observable<IBackendInfo>;
   frontendGitCommitHash = this.stateService.env.GIT_COMMIT_HASH;
   packetJsonVersion = this.stateService.env.PACKAGE_JSON_VERSION;
 
   constructor(
     public stateService: StateService,
-    private navigationService: NavigationService,
   ) {}
 
   ngOnInit(): void {
     this.env = this.stateService.env;
-    this.networkPaths$ = this.navigationService.subnetPaths;
     this.backendInfo$ = this.stateService.backendInfo$;
   }
 
