9c2e7a4ba6bfe52aca43b1cbe7811aba1a5c51e8
Felipe Knorr Kuhn <100320+knorrium@users.noreply.github.com>
2023-05-11 21:36:21 -0700

    Change Liquid tests to fmt host

--- frontend/proxy.conf.staging.js
+++ frontend/proxy.conf.staging.js
@@ -4,7 +4,7 @@ let PROXY_CONFIG = require('./proxy.conf');
 
 PROXY_CONFIG.forEach(entry => {
   entry.target = entry.target.replace("mempool.space", "mempool-staging.fra.mempool.space");
-  entry.target = entry.target.replace("liquid.network", "liquid-staging.fra.mempool.space");
+  entry.target = entry.target.replace("liquid.network", "liquid-staging.fmt.mempool.space");
   entry.target = entry.target.replace("bisq.markets", "bisq-staging.fra.mempool.space");
 });
 
