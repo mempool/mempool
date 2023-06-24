4da00c3c970824548e413d9a227243cb73b4500e
Mononaut <github@monospace.live>
2023-06-23 20:41:39 -0400

    Refactor rust code for style

--- backend/rust-gbt/src/thread_transaction.rs
+++ backend/rust-gbt/src/thread_transaction.rs
@@ -1,46 +1,44 @@
-// use neon::{types::{JsObject, JsNumber, JsArray, JsValue, JsBoolean, JsArrayBuffer, buffer::TypedArray}, prelude::{Object, FunctionContext, Handle}};
-extern crate bytes;
-use std::io::Cursor;
 use bytes::buf::Buf;
+use std::io::Cursor;
 
 pub struct ThreadTransaction {
-  pub uid: u32,
-  pub fee: u64,
-  pub weight: u32,
-  pub sigops: u32,
-  pub fee_per_vsize: f64,
-  pub effective_fee_per_vsize: f64,
-  pub inputs: Vec<u32>,
+    pub uid: u32,
+    pub fee: u64,
+    pub weight: u32,
+    pub sigops: u32,
+    pub fee_per_vsize: f64,
+    pub effective_fee_per_vsize: f64,
+    pub inputs: Vec<u32>,
 }
 
 impl ThreadTransaction {
-  pub fn batch_from_buffer(buffer: &[u8]) -> Vec<ThreadTransaction> {
-    let mut transactions: Vec<ThreadTransaction> = Vec::new();
-    let mut cursor = Cursor::new(buffer);
-    let size = cursor.get_u32();
-    for _ in 0..size {
-      let uid = cursor.get_u32();
-      let fee = cursor.get_f64() as u64;
-      let weight = cursor.get_u32();
-      let sigops = cursor.get_u32();
-      let fee_per_vsize = cursor.get_f64();
-      let effective_fee_per_vsize = cursor.get_f64();
-      let input_count = cursor.get_u32();
-      let mut inputs: Vec<u32> = Vec::new();
-      for _ in 0..input_count {
-        inputs.push(cursor.get_u32());
-      }
-      transactions.push(ThreadTransaction {
-        uid,
-        fee,
-        weight,
-        sigops,
-        fee_per_vsize,
-        effective_fee_per_vsize,
-        inputs,
-      })
-    }
+    pub fn batch_from_buffer(buffer: &[u8]) -> Vec<ThreadTransaction> {
+        let mut transactions: Vec<ThreadTransaction> = Vec::new();
+        let mut cursor = Cursor::new(buffer);
+        let size = cursor.get_u32();
+        for _ in 0..size {
+            let uid = cursor.get_u32();
+            let fee = cursor.get_f64().round() as u64;
+            let weight = cursor.get_u32();
+            let sigops = cursor.get_u32();
+            let fee_per_vsize = cursor.get_f64();
+            let effective_fee_per_vsize = cursor.get_f64();
+            let input_count = cursor.get_u32();
+            let mut inputs: Vec<u32> = Vec::new();
+            for _ in 0..input_count {
+                inputs.push(cursor.get_u32());
+            }
+            transactions.push(ThreadTransaction {
+                uid,
+                fee,
+                weight,
+                sigops,
+                fee_per_vsize,
+                effective_fee_per_vsize,
+                inputs,
+            })
+        }
 
-    return transactions;
-  }
+        transactions
+    }
 }
--- backend/rust-gbt/src/utils.rs
+++ backend/rust-gbt/src/utils.rs
@@ -1,14 +1,13 @@
-extern crate bytes;
-use std::io::Cursor;
 use bytes::buf::Buf;
+use std::io::Cursor;
 
 pub fn txids_from_buffer(buffer: &[u8]) -> Vec<u32> {
-  let mut txids: Vec<u32> = Vec::new();
-  let mut cursor = Cursor::new(buffer);
-  let size = cursor.get_u32();
-  for _ in 0..size {
-    txids.push(cursor.get_u32());
-  }
+    let mut txids: Vec<u32> = Vec::new();
+    let mut cursor: Cursor<&[u8]> = Cursor::new(buffer);
+    let size: u32 = cursor.get_u32();
+    for _ in 0..size {
+        txids.push(cursor.get_u32());
+    }
 
-  return txids;
+    txids
 }
