213800f563a2731a991cfbe2ce76b3ec419a92ad
softsimon <softsimon@protonmail.com>
2023-07-19 16:46:02 +0900

    Merge error fix

--- frontend/src/app/components/search-form/search-form.component.ts
+++ frontend/src/app/components/search-form/search-form.component.ts
@@ -35,10 +35,10 @@ export class SearchFormComponent implements OnInit {
     }
   }
 
-  regexAddress = /^([a-km-zA-HJ-NP-Z1-9]{26,35}|[a-km-zA-HJ-NP-Z1-9]{80}|[A-z]{2,5}1[a-zA-HJ-NP-Z0-9]{39,59})$/;
-  regexBlockhash = /^[0]{8}[a-fA-F0-9]{56}$/;
-  regexTransaction = /^([a-fA-F0-9]{64})(:\d+)?$/;
-  regexBlockheight = /^[0-9]{1,9}$/;
+  regexAddress = getRegex('address', 'mainnet'); // Default to mainnet
+  regexBlockhash = getRegex('blockhash');
+  regexTransaction = getRegex('transaction');
+  regexBlockheight = getRegex('blockheight');
 
   focus$ = new Subject<string>();
   click$ = new Subject<string>();
