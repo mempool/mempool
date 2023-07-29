bb7f73637bea426245280fc3b527f4d79cc63b68
softsimon <softsimon@users.noreply.github.com>
2023-07-29 16:34:47 +0900

    Update backend/src/api/prices/prices.routes.ts

--- backend/src/api/prices/prices.routes.ts
+++ backend/src/api/prices/prices.routes.ts
@@ -7,7 +7,7 @@ class PricesRoutes {
     app.get(config.MEMPOOL.API_URL_PREFIX + 'prices', this.$getCurrentPrices.bind(this));
   }
 
-  private async $getCurrentPrices(req: Request, res: Response): Promise<void> {
+  private $getCurrentPrices(req: Request, res: Response): void {
     res.header('Pragma', 'public');
     res.header('Cache-control', 'public');
     res.setHeader('Expires', new Date(Date.now() + 1000 * 300).toUTCString());
