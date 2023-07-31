86af61042aabd06b05e7c93cb18ac43421df7409
Felipe Knorr Kuhn <git@knorrium.info>
2023-07-31 14:30:44 -0700

    Uncomment geoip-data that was commented out by mistake

--- docker/init.sh
+++ docker/init.sh
@@ -4,9 +4,9 @@
 cp ./docker/backend/* ./backend/
 
 #geoip-data
-#mkdir -p ./backend/GeoIP/
-#wget -O ./backend/GeoIP/GeoLite2-City.mmdb https://raw.githubusercontent.com/mempool/geoip-data/master/GeoLite2-City.mmdb
-#wget -O ./backend/GeoIP/GeoLite2-ASN.mmdb https://raw.githubusercontent.com/mempool/geoip-data/master/GeoLite2-ASN.mmdb
+mkdir -p ./backend/GeoIP/
+wget -O ./backend/GeoIP/GeoLite2-City.mmdb https://raw.githubusercontent.com/mempool/geoip-data/master/GeoLite2-City.mmdb
+wget -O ./backend/GeoIP/GeoLite2-ASN.mmdb https://raw.githubusercontent.com/mempool/geoip-data/master/GeoLite2-ASN.mmdb
 
 #frontend
 localhostIP="127.0.0.1"
