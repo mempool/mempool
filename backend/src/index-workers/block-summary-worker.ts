e3bc33e41b09f1d78736b2ae4f5300fa38034e64
Mononaut <github@monospace.live>
2023-08-02 20:09:02 +0900

    Always use Core verbose block for summaries indexing

--- backend/src/index-workers/block-summary-worker.ts
+++ backend/src/index-workers/block-summary-worker.ts
@@ -18,20 +18,15 @@ if (parentPort) {
 }
 
 async function indexBlockSummary(hash: string, height: number): Promise<void> {
-  let txs;
-  if (config.MEMPOOL.BACKEND === 'esplora') {
-    txs = (await bitcoinApi.$getTxsForBlock(hash)).map(tx => transactionUtils.extendTransaction(tx));
-  } else {
-    const block = await bitcoinClient.getBlock(hash, 2);
-    txs = block.tx.map(tx => {
-      tx.fee = Math.round(tx.fee * 100_000_000);
-      tx.vout.forEach((vout) => {
-        vout.value = Math.round(vout.value * 100000000);
-      });
-      tx.vsize = Math.round(tx.weight / 4); // required for backwards compatibility
-      return tx;
+  const block = await bitcoinClient.getBlock(hash, 2);
+  const txs = block.tx.map(tx => {
+    tx.fee = Math.round(tx.fee * 100_000_000);
+    tx.vout.forEach((vout) => {
+      vout.value = Math.round(vout.value * 100000000);
     });
-  }
+    tx.vsize = Math.round(tx.weight / 4); // required for backwards compatibility
+    return tx;
+  });
 
   const cpfpSummary = await blocks.$indexCPFP(hash, height, txs);
   await blocks.$getStrippedBlockTransactions(hash, true, true, cpfpSummary, height); // This will index the block summary
