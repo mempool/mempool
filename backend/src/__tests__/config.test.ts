7dad00523ff53a9ad49730c3421b880bdaecc979
Mononaut <github@monospace.live>
2023-08-28 19:20:58 +0900

    Implement pid file & checks

--- backend/src/__tests__/config.test.ts
+++ backend/src/__tests__/config.test.ts
@@ -84,6 +84,7 @@ describe('Mempool Backend Config', () => {
         USERNAME: 'mempool',
         PASSWORD: 'mempool',
         TIMEOUT: 180000,
+        PID_DIR: ''
       });
 
       expect(config.SYSLOG).toStrictEqual({
