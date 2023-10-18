10e468d463b8926429aef7e18c200b38edeb1c2d
Mononaut <github@monospace.live>
2023-10-18 21:14:59 +0000

    Fix hashrate difficulty SSR state dependency

--- frontend/src/app/components/hashrate-chart/hashrate-chart.component.ts
+++ frontend/src/app/components/hashrate-chart/hashrate-chart.component.ts
@@ -326,7 +326,7 @@ export class HashrateChartComponent implements OnInit {
             },
           },
         ],
-        selected: JSON.parse(this.storageService.getValue('hashrate_difficulty_legend')) ?? {
+        selected: JSON.parse(this.storageService?.getValue('hashrate_difficulty_legend') || 'null') ?? {
           '$localize`:@@79a9dc5b1caca3cbeb1733a19515edacc5fc7920:Hashrate`': true,
           '$localize`::Difficulty`': this.network === '',
           '$localize`Hashrate (MA)`': true,
