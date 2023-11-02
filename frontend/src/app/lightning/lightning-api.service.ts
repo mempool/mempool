b167848b9b65871797448222675def0713fffc7a
Mononaut <github@monospace.live>
2023-11-02 23:26:17 +0000

    SSR: Fix initial state of lightning pages & graphs

--- frontend/src/app/lightning/lightning-api.service.ts
+++ frontend/src/app/lightning/lightning-api.service.ts
@@ -8,6 +8,7 @@ import { IChannel, INodesRanking, IOldestNodes, ITopNodesPerCapacity, ITopNodesP
   providedIn: 'root'
 })
 export class LightningApiService {
+  private apiBaseUrl: string; // base URL is protocol, hostname, and port
   private apiBasePath = ''; // network path is /testnet, etc. or '' for mainnet
   
   private requestCache = new Map<string, { subject: BehaviorSubject<any>, expiry: number }>;
@@ -16,6 +17,10 @@ export class LightningApiService {
     private httpClient: HttpClient,
     private stateService: StateService,
   ) {
+    this.apiBaseUrl = ''; // use relative URL by default
+    if (!stateService.isBrowser) { // except when inside AU SSR process
+      this.apiBaseUrl = this.stateService.env.NGINX_PROTOCOL + '://' + this.stateService.env.NGINX_HOSTNAME + ':' + this.stateService.env.NGINX_PORT;
+    }
     this.apiBasePath = ''; // assume mainnet by default
     this.stateService.networkChanged$.subscribe((network) => {
       if (network === 'bisq' && !this.stateService.env.BISQ_SEPARATE_BACKEND) {
@@ -66,15 +71,15 @@ export class LightningApiService {
   }
 
   getNode$(publicKey: string): Observable<any> {
-    return this.httpClient.get<any>(this.apiBasePath + '/api/v1/lightning/nodes/' + publicKey);
+    return this.httpClient.get<any>(this.apiBaseUrl + this.apiBasePath + '/api/v1/lightning/nodes/' + publicKey);
   }
 
   getNodeGroup$(name: string): Observable<any[]> {
-    return this.httpClient.get<any[]>(this.apiBasePath + '/api/v1/lightning/nodes/group/' + name);
+    return this.httpClient.get<any[]>(this.apiBaseUrl + this.apiBasePath + '/api/v1/lightning/nodes/group/' + name);
   }
 
   getChannel$(shortId: string): Observable<any> {
-    return this.httpClient.get<any>(this.apiBasePath + '/api/v1/lightning/channels/' + shortId);
+    return this.httpClient.get<any>(this.apiBaseUrl + this.apiBasePath + '/api/v1/lightning/channels/' + shortId);
   }
 
   getChannelsByNodeId$(publicKey: string, index: number = 0, status = 'open'): Observable<any> {
@@ -84,57 +89,57 @@ export class LightningApiService {
       .set('status', status)
     ;
 
-    return this.httpClient.get<any>(this.apiBasePath + '/api/v1/lightning/channels', { params, observe: 'response' });
+    return this.httpClient.get<any>(this.apiBaseUrl + this.apiBasePath + '/api/v1/lightning/channels', { params, observe: 'response' });
   }
 
   getLatestStatistics$(): Observable<any> {
-    return this.httpClient.get<any>(this.apiBasePath + '/api/v1/lightning/statistics/latest');
+    return this.httpClient.get<any>(this.apiBaseUrl + this.apiBasePath + '/api/v1/lightning/statistics/latest');
   }
 
   listNodeStats$(publicKey: string): Observable<any> {
-    return this.httpClient.get<any>(this.apiBasePath + '/api/v1/lightning/nodes/' + publicKey + '/statistics');
+    return this.httpClient.get<any>(this.apiBaseUrl + this.apiBasePath + '/api/v1/lightning/nodes/' + publicKey + '/statistics');
   }
 
   getNodeFeeHistogram$(publicKey: string): Observable<any> {
-    return this.httpClient.get<any>(this.apiBasePath + '/api/v1/lightning/nodes/' + publicKey + '/fees/histogram');
+    return this.httpClient.get<any>(this.apiBaseUrl + this.apiBasePath + '/api/v1/lightning/nodes/' + publicKey + '/fees/histogram');
   }
 
   getNodesRanking$(): Observable<INodesRanking> {
-    return this.httpClient.get<INodesRanking>(this.apiBasePath + '/api/v1/lightning/nodes/rankings');
+    return this.httpClient.get<INodesRanking>(this.apiBaseUrl + this.apiBasePath + '/api/v1/lightning/nodes/rankings');
   }
 
   listChannelStats$(publicKey: string): Observable<any> {
-    return this.httpClient.get<any>(this.apiBasePath + '/channels/' + publicKey + '/statistics');
+    return this.httpClient.get<any>(this.apiBaseUrl + this.apiBasePath + '/channels/' + publicKey + '/statistics');
   }
 
   listStatistics$(interval: string | undefined): Observable<any> {
     return this.httpClient.get<any>(
-      this.apiBasePath + '/api/v1/lightning/statistics' +
+      this.apiBaseUrl + this.apiBasePath + '/api/v1/lightning/statistics' +
       (interval !== undefined ? `/${interval}` : ''), { observe: 'response' }
     );
   }
 
   getTopNodesByCapacity$(): Observable<ITopNodesPerCapacity[]> {
     return this.httpClient.get<ITopNodesPerCapacity[]>(
-      this.apiBasePath + '/api/v1/lightning/nodes/rankings/liquidity'
+      this.apiBaseUrl + this.apiBasePath + '/api/v1/lightning/nodes/rankings/liquidity'
     );
   }
 
   getTopNodesByChannels$(): Observable<ITopNodesPerChannels[]> {
     return this.httpClient.get<ITopNodesPerChannels[]>(
-      this.apiBasePath + '/api/v1/lightning/nodes/rankings/connectivity'
+      this.apiBaseUrl + this.apiBasePath + '/api/v1/lightning/nodes/rankings/connectivity'
     );
   }
 
   getPenaltyClosedChannels$(): Observable<IChannel[]> {
     return this.httpClient.get<IChannel[]>(
-      this.apiBasePath + '/api/v1/lightning/penalties'
+      this.apiBaseUrl + this.apiBasePath + '/api/v1/lightning/penalties'
     );
   }
 
   getOldestNodes$(): Observable<IOldestNodes[]> {
     return this.httpClient.get<IOldestNodes[]>(
-      this.apiBasePath + '/api/v1/lightning/nodes/rankings/age'
+      this.apiBaseUrl + this.apiBasePath + '/api/v1/lightning/nodes/rankings/age'
     );
   }
 }
