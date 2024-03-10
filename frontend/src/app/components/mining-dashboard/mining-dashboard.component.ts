a5f864fe3508f3927fef76d0b7079291b20667fb
hunicus <93150691+hunicus@users.noreply.github.com>
2024-03-10 11:10:24 +0900

    Explicitly set mining and lightning preview images

--- frontend/src/app/components/mining-dashboard/mining-dashboard.component.ts
+++ frontend/src/app/components/mining-dashboard/mining-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, ChangeDetectionStrategy, Component, HostListener, OnInit } from '@angular/core';
 import { SeoService } from '../../services/seo.service';
+import { OpenGraphService } from '../../services/opengraph.service';
 import { WebsocketService } from '../../services/websocket.service';
 import { StateService } from '../../services/state.service';
 import { EventType, NavigationStart, Router } from '@angular/router';
@@ -15,12 +16,14 @@ export class MiningDashboardComponent implements OnInit, AfterViewInit {
 
   constructor(
     private seoService: SeoService,
+    private ogService: OpenGraphService,
     private websocketService: WebsocketService,
     private stateService: StateService,
     private router: Router
   ) {
     this.seoService.setTitle($localize`:@@a681a4e2011bb28157689dbaa387de0dd0aa0c11:Mining Dashboard`);
     this.seoService.setDescription($localize`:@@meta.description.mining.dashboard:Get real-time Bitcoin mining stats like hashrate, difficulty adjustment, block rewards, pool dominance, and more.`);
+    this.ogService.setManualOgImage('mining.jpg');
   }
 
   ngOnInit(): void {
