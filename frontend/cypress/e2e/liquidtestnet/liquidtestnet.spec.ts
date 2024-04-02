3e3bed9aa4c093308e103edde5eaa02f4a503918
Felipe Knorr Kuhn <git@knorrium.info>
2024-04-02 16:54:02 +0900

    Disable viewport tests for now

--- frontend/cypress/e2e/liquidtestnet/liquidtestnet.spec.ts
+++ frontend/cypress/e2e/liquidtestnet/liquidtestnet.spec.ts
@@ -28,7 +28,7 @@ describe('Liquid Testnet', () => {
       cy.waitForSkeletonGone();
     });
 
-    it('loads the dashboard with no scrollbars on mobile', () => {
+    it.skip('loads the dashboard with no scrollbars on mobile', () => {
       cy.viewport('iphone-xr');
       cy.visit(`${basePath}`);
       cy.waitForSkeletonGone();
@@ -68,7 +68,7 @@ describe('Liquid Testnet', () => {
       cy.get('.tv-only').should('not.exist');
     });
 
-    it('renders unconfidential transactions correctly on mobile', () => {
+    it.skip('renders unconfidential transactions correctly on mobile', () => {
       cy.viewport('iphone-xr');
       cy.visit(`${basePath}/tx/b119f338878416781dc285b94c0de52826341dea43566e4de4740d3ebfd1f6dc#blinded=99707,144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49,1377e4ec8eb0c89296e14ffca57e377f4b51ad8f1c881e43364434d8430dbfda,cdd6caae4c3452586cfcb107478dd2b7acaa5f82714a6a966578255e857eee60`);
       cy.waitForSkeletonGone();
