1bba1cfeb138bb4ca899edc32621c7b9e9d93576
nymkappa <1612910616@pm.me>
2024-04-02 11:35:03 +0800

    [accelerator] deposit -> top up

--- frontend/src/app/shared/components/mempool-error/mempool-error.component.ts
+++ frontend/src/app/shared/components/mempool-error/mempool-error.component.ts
@@ -14,7 +14,7 @@ const MempoolErrors = {
   'mempool_rejected_raw_tx': `Our mempool rejected this transaction`,
   'no_mining_pool_available': `No mining pool available at the moment`,
   'not_available': `You current subscription does not allow you to access this feature.`,
-  'not_enough_balance': `Your account balance is too low. Please make a <a style="color:#105fb0" href="/services/accelerator/overview">deposit.</a>`,
+  'not_enough_balance': `Your balance is too low. Please <a style="color:#105fb0" href="/services/accelerator/overview">top up your account</a>.`,
   'not_verified': `You must verify your account to use this feature.`,
   'recommended_fees_not_available': `Recommended fees are not available right now.`,
   'too_many_relatives': `This transaction has too many relatives.`,
