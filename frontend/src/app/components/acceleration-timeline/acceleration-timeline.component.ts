7c08a104ce110e3f4abdfc497b450df7e25e4bfb
natsoni <natsoni@protonmail.com>
2024-07-05 16:48:50 +0900

    remove rtl for now

--- frontend/src/app/components/acceleration-timeline/acceleration-timeline.component.ts
+++ frontend/src/app/components/acceleration-timeline/acceleration-timeline.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, OnChanges, Inject, LOCALE_ID } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
 import { ETA } from '../../services/eta.service';
 import { Transaction } from '../../interfaces/electrs.interface';
 
@@ -13,15 +13,8 @@ export class AccelerationTimelineComponent implements OnInit, OnChanges {
   @Input() eta: ETA;
 
   acceleratedAt: number;
-  dir: 'rtl' | 'ltr' = 'ltr';
 
-  constructor(
-    @Inject(LOCALE_ID) private locale: string,
-  ) {
-    if (this.locale.startsWith('ar') || this.locale.startsWith('fa') || this.locale.startsWith('he')) {
-      this.dir = 'rtl';
-    }
-  }
+  constructor() {}
 
   ngOnInit(): void {
     this.acceleratedAt = this.tx.acceleratedAt ?? new Date().getTime() / 1000;
