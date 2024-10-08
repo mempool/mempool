15b3c88a1f56cdbd98a77b326ee2da4d5e9a4a7a
Mononaut <github@monospace.live>
2024-10-08 02:40:14 +0000

    fix optional rune divisibility bug

--- frontend/src/app/shared/ord/rune.utils.ts
+++ frontend/src/app/shared/ord/rune.utils.ts
@@ -201,9 +201,9 @@ function messageToRunestone(message: Message): Runestone {
   if (flags & Flag.ETCHING) {
     const hasTerms = (flags & Flag.TERMS) > 0n;
     const isTurbo = (flags & Flag.TURBO) > 0n;
-    const name = parseRuneName(message.fields[Tag.Rune][0]);
+    const name = parseRuneName(message.fields[Tag.Rune]?.[0] ?? 0n);
     etching = {
-      divisibility: Number(message.fields[Tag.Divisibility][0]),
+      divisibility: Number(message.fields[Tag.Divisibility]?.[0] ?? 0n),
       premine: message.fields[Tag.Premine]?.[0],
       symbol: message.fields[Tag.Symbol]?.[0] ? String.fromCodePoint(Number(message.fields[Tag.Symbol][0])) : '¤',
       terms: hasTerms ? {
