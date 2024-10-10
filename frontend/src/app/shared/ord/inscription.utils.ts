69a994afd59c3c7fc492d29932b40d678fdafc99
natsoni <natsoni@protonmail.com>
2024-10-10 20:53:19 +0900

    Add license to inscriptions.utils.ts

--- frontend/src/app/shared/ord/inscription.utils.ts
+++ frontend/src/app/shared/ord/inscription.utils.ts
@@ -1,3 +1,19 @@
+/*
+MIT License
+
+Copyright (c) 2024 HAUS HOPPE
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+*/
+
 // Adapted from https://github.com/ordpool-space/ordpool-parser/tree/ce04d7a5b6bb1cf37b9fdadd77ba430f5bd6e7d6/src
 // Utils functions to decode ord inscriptions
 
