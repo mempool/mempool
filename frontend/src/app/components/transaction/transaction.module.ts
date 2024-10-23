23475c7a1b4223321b1a8dfa09c4ed2546a8096e
Mononaut <github@monospace.live>
2024-10-23 08:56:27 +0000

    Refactor transaction page component

--- frontend/src/app/components/transaction/transaction.module.ts
+++ frontend/src/app/components/transaction/transaction.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { TransactionComponent } from '@components/transaction/transaction.component';
+import { TransactionDetailsComponent } from '@components/transaction/transaction-details/transaction-details.component';
 import { SharedModule } from '@app/shared/shared.module';
 import { TxBowtieModule } from '@components/tx-bowtie-graph/tx-bowtie.module';
+import { TransactionExtrasModule } from '@components/transaction/transaction-extras.module';
 import { GraphsModule } from '@app/graphs/graphs.module';
 import { AccelerateCheckout } from '@components/accelerate-checkout/accelerate-checkout.component';
 import { AccelerateFeeGraphComponent } from '@components/accelerate-checkout/accelerate-fee-graph.component';
@@ -40,14 +42,17 @@ export class TransactionRoutingModule { }
     SharedModule,
     GraphsModule,
     TxBowtieModule,
+    TransactionExtrasModule,
   ],
   declarations: [
     TransactionComponent,
+    TransactionDetailsComponent,
     AccelerateCheckout,
     AccelerateFeeGraphComponent,
   ],
   exports: [
     TransactionComponent,
+    TransactionDetailsComponent,
     AccelerateCheckout,
     AccelerateFeeGraphComponent,
   ]
