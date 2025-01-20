227d99e9909d1401867f3cb6258e2c4c4b05ead9
Felipe Knorr Kuhn <git@knorrium.info>
2025-01-19 19:35:08 -0800

    Fix reroute logic

--- frontend/proxy.conf.staging.js
+++ frontend/proxy.conf.staging.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 let PROXY_CONFIG = require('./proxy.conf');
 
 PROXY_CONFIG.forEach(entry => {
-  const hostname = process.env.CYPRESS_REROUTE_TESTNET ?  'node201.fmt.mempool.space' : 'mempool-staging.fra.mempool.space';
+  const hostname = process.env.CYPRESS_REROUTE_TESTNET === 'true' ? 'mempool-staging.fra.mempool.space' : 'node201.fmt.mempool.space';
+  console.log(`e2e tests running against ${hostname}`);
   entry.target = entry.target.replace("mempool.space", hostname);
   entry.target = entry.target.replace("liquid.network", "liquid-staging.fmt.mempool.space");
 });
