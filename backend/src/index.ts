aad4783415f791ec50ef8fb8989a4312d05a91fe
nymkappa <1612910616@pm.me>
2025-02-04 21:33:21 +0100

    fix cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -131,6 +131,9 @@ class Server {
     this.app
       .use((req: Request, res: Response, next: NextFunction) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Requested-With');
+        res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count,X-Mempool-Auth');
         next();
       })
       .use(express.urlencoded({ extended: true }))
