fb50ea7a6d06e3a4a28db3061d8b2ba74bedafee
Mononaut <github@monospace.live>
2025-02-04 12:02:12 +0000

    detect and warn about address poisoning attacks

---
+++ frontend/src/app/shared/components/address-text/address-text.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { AddressMatch, AddressTypeInfo } from '@app/shared/address-utils';
+
+@Component({
+  selector: 'app-address-text',
+  templateUrl: './address-text.component.html',
+  styleUrls: ['./address-text.component.scss']
+})
+export class AddressTextComponent {
+  @Input() address: string;
+  @Input() info: AddressTypeInfo | null;
+  @Input() similarity: { score: number, match: AddressMatch, group: number } | null;
+
+  groupColors: string[] = [
+    'var(--primary)',
+    'var(--success)',
+    'var(--info)',
+    'white',
+  ];
+}
