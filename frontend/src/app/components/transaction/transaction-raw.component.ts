4dff3adf1169d056f35acd5b3c28bb5fadbd850d
natsoni <natsoni@protonmail.com>
2025-03-12 16:49:38 +0100

    Redirect to tx page 2 seconds after broadcast

--- frontend/src/app/components/transaction/transaction-raw.component.ts
+++ frontend/src/app/components/transaction/transaction-raw.component.ts
@@ -3,7 +3,7 @@ import { Transaction, Vout } from '@interfaces/electrs.interface';
 import { StateService } from '../../services/state.service';
 import { Filter, toFilters } from '../../shared/filters.utils';
 import { decodeRawTransaction, getTransactionFlags, addInnerScriptsToVin, countSigops } from '../../shared/transaction.utils';
-import { firstValueFrom, Subscription } from 'rxjs';
+import { catchError, firstValueFrom, Subscription, switchMap, tap, throwError, timer } from 'rxjs';
 import { WebsocketService } from '../../services/websocket.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
@@ -36,6 +36,7 @@ export class TransactionRawComponent implements OnInit, OnDestroy {
   isLoadingBroadcast: boolean;
   errorBroadcast: string;
   successBroadcast: boolean;
+  broadcastSubscription: Subscription;
 
   isMobile: boolean;
   @ViewChild('graphContainer')
@@ -207,18 +208,22 @@ export class TransactionRawComponent implements OnInit, OnDestroy {
     });
   }
 
-  async postTx(): Promise<string> {
+  postTx(): void {
     this.isLoadingBroadcast = true;
     this.errorBroadcast = null;
-    return new Promise((resolve, reject) => {
-      this.apiService.postTransaction$(this.rawHexTransaction)
-      .subscribe((result) => {
+
+    this.broadcastSubscription = this.apiService.postTransaction$(this.rawHexTransaction).pipe(
+      tap((txid: string) => {
         this.isLoadingBroadcast = false;
         this.successBroadcast = true;
-        this.transaction.txid = result;
-        resolve(result);
-      },
-      (error) => {
+        this.transaction.txid = txid;
+      }),
+      switchMap((txid: string) =>
+        timer(2000).pipe(
+          tap(() => this.router.navigate([this.relativeUrlPipe.transform('/tx/' + txid)])),
+        )
+      ),
+      catchError((error) => {
         if (typeof error.error === 'string') {
           const matchText = error.error.replace(/\\/g, '').match('"message":"(.*?)"');
           this.errorBroadcast = 'Failed to broadcast transaction, reason: ' + (matchText && matchText[1] || error.error);
@@ -226,9 +231,9 @@ export class TransactionRawComponent implements OnInit, OnDestroy {
           this.errorBroadcast = 'Failed to broadcast transaction, reason: ' + error.message;
         }
         this.isLoadingBroadcast = false;
-        reject(this.error);
-      });
-    });
+        return throwError(() => error);
+      })
+    ).subscribe();
   }
 
   resetState() {
@@ -253,6 +258,7 @@ export class TransactionRawComponent implements OnInit, OnDestroy {
     this.missingPrevouts = [];
     this.stateService.markBlock$.next({});
     this.mempoolBlocksSubscription?.unsubscribe();
+    this.broadcastSubscription?.unsubscribe();
   }
 
   resetForm() {
@@ -308,6 +314,7 @@ export class TransactionRawComponent implements OnInit, OnDestroy {
     this.mempoolBlocksSubscription?.unsubscribe();
     this.flowPrefSubscription?.unsubscribe();
     this.stateService.markBlock$.next({});
+    this.broadcastSubscription?.unsubscribe();
   }
 
 }
