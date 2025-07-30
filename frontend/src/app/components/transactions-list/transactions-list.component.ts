45f126a5fa45b85df983ace5b23875449e6b956c
mononaut <83316221+mononaut@users.noreply.github.com>
2025-07-30 08:52:09 +0000

    standardize taproot witness treatment

--- frontend/src/app/components/transactions-list/transactions-list.component.ts
+++ frontend/src/app/components/transactions-list/transactions-list.component.ts
@@ -342,6 +342,7 @@ export class TransactionsListComponent implements OnInit, OnChanges, OnDestroy {
               const isScriptSpend = vin.witness.length > (hasAnnex ? 2 : 1);
               if (isScriptSpend) {
                 const controlBlock = hasAnnex ? vin.witness[vin.witness.length - 2] : vin.witness[vin.witness.length - 1];
+                const scriptHex = hasAnnex ? vin.witness[vin.witness.length - 3] : vin.witness[vin.witness.length - 2]; // bip341 script element
                 const tapleafVersion = parseInt(controlBlock.slice(0, 2), 16) & 0xfe;
                 // simplicity script spend
                 if (tapleafVersion === 0xbe) {
