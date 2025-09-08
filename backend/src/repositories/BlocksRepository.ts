9cafdb2711c104b599c9d1fb5e700fe46a18c4e1
Mononaut <github@monospace.live>
2025-09-08 09:01:08 +0000

    Support stales in db blocks table

--- backend/src/repositories/BlocksRepository.ts
+++ backend/src/repositories/BlocksRepository.ts
@@ -60,6 +60,7 @@ interface DatabaseBlock {
   utxoSetSize: number;
   totalInputAmt: number;
   firstSeen: number;
+  stale: boolean;
 }
 
 const BLOCK_DB_FIELDS = `
@@ -104,7 +105,8 @@ const BLOCK_DB_FIELDS = `
   blocks.utxoset_change AS utxoSetChange,
   blocks.utxoset_size AS utxoSetSize,
   blocks.total_input_amt AS totalInputAmt,
-  UNIX_TIMESTAMP(blocks.first_seen) AS firstSeen
+  UNIX_TIMESTAMP(blocks.first_seen) AS firstSeen,
+  blocks.stale
 `;
 
 class BlocksRepository {
@@ -128,7 +130,8 @@ class BlocksRepository {
         coinbase_signature, utxoset_size,             utxoset_change,    avg_tx_size,
         total_inputs,       total_outputs,            total_input_amt,   total_output_amt,
         fee_percentiles,    segwit_total_txs,         segwit_total_size, segwit_total_weight,
-        median_fee_amt,     coinbase_signature_ascii, definition_hash,   index_version
+        median_fee_amt,     coinbase_signature_ascii, definition_hash,   index_version,
+        stale
       ) VALUE (
         ?, ?, FROM_UNIXTIME(?), ?,
         ?, ?, ?, ?,
@@ -139,7 +142,8 @@ class BlocksRepository {
         ?, ?, ?, ?,
         ?, ?, ?, ?,
         ?, ?, ?, ?,
-        ?, ?, ?, ?
+        ?, ?, ?, ?,
+        ?
       )`;
 
       const poolDbId = await PoolsRepository.$getPoolByUniqueId(block.extras.pool.id);
@@ -187,13 +191,23 @@ class BlocksRepository {
         block.extras.medianFeeAmt,
         truncatedCoinbaseSignatureAscii,
         poolsUpdater.currentSha,
-        BlocksRepository.version
+        BlocksRepository.version,
+        (block.stale ? 1 : 0),
       ];
 
       await DB.query(query, params);
     } catch (e: any) {
-      if (e.errno === 1062) { // ER_DUP_ENTRY - This scenario is possible upon node backend restart
-        logger.debug(`$saveBlockInDatabase() - Block ${block.height} has already been indexed, ignoring`, logger.tags.mining);
+      if (e.errno === 1062) { // ER_DUP_ENTRY - This scenario is possible upon node backend restart or if a stale block is reconnected
+        if (!block.stale) {
+          logger.debug(`$saveBlockInDatabase() - Block ${block.height} has already been indexed, setting as canonical`, logger.tags.mining);
+          try {
+            await this.$setCanonicalBlockAtHeight(block.id, block.height);
+          } catch (e: any) {
+            logger.err(`Cannot set canonical block at height ${block.height}. Reason: ` + (e instanceof Error ? e.message : e));
+          }
+        } else {
+          logger.debug(`$saveBlockInDatabase() - Block ${block.height} has already been indexed, ignoring`, logger.tags.mining);
+        }
       } else {
         logger.err('Cannot save indexed block into db. Reason: ' + (e instanceof Error ? e.message : e), logger.tags.mining);
         throw e;
@@ -266,7 +280,7 @@ class BlocksRepository {
       const [rows]: any[] = await DB.query(`
         SELECT height
         FROM blocks
-        WHERE height <= ? AND height >= ?
+        WHERE height <= ? AND height >= ? AND stale = 0
         ORDER BY height DESC;
       `, [startHeight, endHeight]);
 
@@ -292,7 +306,7 @@ class BlocksRepository {
     let query = `SELECT count(height) as count, pools.id as poolId
       FROM blocks
       JOIN pools on pools.id = blocks.pool_id
-      WHERE tx_count = 1`;
+      WHERE tx_count = 1 AND stale = 0`;
 
     if (poolId) {
       query += ` AND pool_id = ?`;
@@ -335,20 +349,16 @@ class BlocksRepository {
 
     const params: any[] = [];
     let query = `SELECT count(height) as blockCount
-      FROM blocks`;
+      FROM blocks 
+      WHERE stale = 0`;
 
     if (poolId) {
-      query += ` WHERE pool_id = ?`;
+      query += ` AND pool_id = ?`;
       params.push(poolId);
     }
 
     if (interval) {
-      if (poolId) {
-        query += ` AND`;
-      } else {
-        query += ` WHERE`;
-      }
-      query += ` blockTimestamp BETWEEN DATE_SUB(NOW(), INTERVAL ${interval}) AND NOW()`;
+      query += ` AND blockTimestamp BETWEEN DATE_SUB(NOW(), INTERVAL ${interval}) AND NOW()`;
     }
 
     try {
@@ -372,19 +382,15 @@ class BlocksRepository {
     let query = `SELECT
       count(height) as blockCount,
       max(height) as lastBlockHeight
-      FROM blocks`;
+      FROM blocks
+      WHERE stale = 0`;
 
     if (poolId) {
-      query += ` WHERE pool_id = ?`;
+      query += ` AND pool_id = ?`;
       params.push(poolId);
     }
 
-    if (poolId) {
-      query += ` AND`;
-    } else {
-      query += ` WHERE`;
-    }
-    query += ` blockTimestamp BETWEEN FROM_UNIXTIME('${from}') AND FROM_UNIXTIME('${to}')`;
+    query += ` AND blockTimestamp BETWEEN FROM_UNIXTIME('${from}') AND FROM_UNIXTIME('${to}')`;
 
     try {
       const [rows] = await DB.query(query, params);
@@ -402,7 +408,7 @@ class BlocksRepository {
     const params: any[] = [];
     let query = `SELECT count(height) as blockCount
       FROM blocks
-      WHERE height <= ${startHeight} AND height >= ${endHeight}`;
+      WHERE height <= ${startHeight} AND height >= ${endHeight} AND stale = 0`;
 
     try {
       const [rows] = await DB.query(query, params);
@@ -422,7 +428,7 @@ class BlocksRepository {
       SELECT AVG(blocks_audits.match_rate) AS avg_match_rate
       FROM blocks
       JOIN blocks_audits ON blocks.height = blocks_audits.height
-      WHERE blocks.pool_id = ?
+      WHERE blocks.pool_id = ? AND stale = 0
     `;
     params.push(poolId);
 
@@ -446,7 +452,7 @@ class BlocksRepository {
     const query = `
       SELECT sum(reward) as total_reward
       FROM blocks
-      WHERE blocks.pool_id = ?
+      WHERE blocks.pool_id = ? AND stale = 0
     `;
     params.push(poolId);
 
@@ -468,6 +474,7 @@ class BlocksRepository {
   public async $oldestBlockTimestamp(): Promise<number> {
     const query = `SELECT UNIX_TIMESTAMP(blockTimestamp) as blockTimestamp
       FROM blocks
+      WHERE stale = 0
       ORDER BY height
       LIMIT 1;`;
 
@@ -499,7 +506,7 @@ class BlocksRepository {
       SELECT ${BLOCK_DB_FIELDS}
       FROM blocks
       JOIN pools ON blocks.pool_id = pools.id
-      WHERE pool_id = ?`;
+      WHERE pool_id = ? AND stale = 0`;
     params.push(pool.id);
 
     if (startHeight !== undefined) {
@@ -534,7 +541,7 @@ class BlocksRepository {
         SELECT ${BLOCK_DB_FIELDS}
         FROM blocks
         JOIN pools ON blocks.pool_id = pools.id
-        WHERE blocks.height = ?`,
+        WHERE blocks.height = ? AND stale = 0`,
         [height]
       );
 
@@ -550,11 +557,35 @@ class BlocksRepository {
   }
 
   /**
+   * Get one block by hash
+   */
+  public async $getBlockByHash(hash: string): Promise<BlockExtended | null> {
+    try {
+      const [rows]: any[] = await DB.query(`
+        SELECT ${BLOCK_DB_FIELDS}
+        FROM blocks
+        JOIN pools ON blocks.pool_id = pools.id
+        WHERE blocks.hash = ?`,
+        [hash]
+      );
+
+      if (rows.length <= 0) {
+        return null;
+      }
+
+      return await this.formatDbBlockIntoExtendedBlock(rows[0] as DatabaseBlock);
+    } catch (e) {
+      logger.err(`Cannot get indexed block ${hash}. Reason: ` + (e instanceof Error ? e.message : e));
+      throw e;
+    }
+  }
+
+  /**
    * Return blocks difficulty
    */
   public async $getBlocksDifficulty(): Promise<object[]> {
     try {
-      const [rows]: any[] = await DB.query(`SELECT UNIX_TIMESTAMP(blockTimestamp) as time, height, difficulty, bits FROM blocks ORDER BY height ASC`);
+      const [rows]: any[] = await DB.query(`SELECT UNIX_TIMESTAMP(blockTimestamp) as time, height, difficulty, bits FROM blocks WHERE stale = 0 ORDER BY height ASC`);
       return rows;
     } catch (e) {
       logger.err('Cannot get blocks difficulty list from the db. Reason: ' + (e instanceof Error ? e.message : e));
@@ -573,7 +604,7 @@ class BlocksRepository {
     try {
       // Get first block at or after the given timestamp
       const query = `SELECT height, hash, blockTimestamp as timestamp FROM blocks
-        WHERE blockTimestamp <= FROM_UNIXTIME(?)
+        WHERE blockTimestamp <= FROM_UNIXTIME(?) AND stale = 0
         ORDER BY blockTimestamp DESC
         LIMIT 1`;
       const params = [timestamp];
@@ -602,6 +633,7 @@ class BlocksRepository {
         SELECT MIN(height) as startBlock, MAX(height) as endBlock, SUM(reward) as totalReward, SUM(fees) as totalFee, SUM(tx_count) as totalTx
         FROM
           (SELECT height, reward, fees, tx_count FROM blocks
+          WHERE stale = 0
           ORDER by height DESC
           LIMIT ?) as sub`;
 
@@ -686,12 +718,13 @@ class BlocksRepository {
         FROM blocks
         JOIN blocks_prices on blocks_prices.height = blocks.height
         JOIN prices on prices.id = blocks_prices.price_id
+        WHERE stale = 0
       `;
 
       if (interval !== null) {
-        query += ` WHERE blockTimestamp BETWEEN DATE_SUB(NOW(), INTERVAL ${interval}) AND NOW()`;
+        query += ` AND blockTimestamp BETWEEN DATE_SUB(NOW(), INTERVAL ${interval}) AND NOW()`;
       } else if (timespan) {
-        query += ` WHERE blockTimestamp BETWEEN FROM_UNIXTIME(${timespan.from}) AND FROM_UNIXTIME(${timespan.to})`;
+        query += ` AND blockTimestamp BETWEEN FROM_UNIXTIME(${timespan.from}) AND FROM_UNIXTIME(${timespan.to})`;
       }
 
       query += ` GROUP BY UNIX_TIMESTAMP(blockTimestamp) DIV ${div}`;
@@ -717,10 +750,11 @@ class BlocksRepository {
         FROM blocks
         JOIN blocks_prices on blocks_prices.height = blocks.height
         JOIN prices on prices.id = blocks_prices.price_id
+        WHERE stale = 0
       `;
 
       if (interval !== null) {
-        query += ` WHERE blockTimestamp BETWEEN DATE_SUB(NOW(), INTERVAL ${interval}) AND NOW()`;
+        query += ` AND blockTimestamp BETWEEN DATE_SUB(NOW(), INTERVAL ${interval}) AND NOW()`;
       }
 
       query += ` GROUP BY UNIX_TIMESTAMP(blockTimestamp) DIV ${div}`;
@@ -748,10 +782,11 @@ class BlocksRepository {
         CAST(AVG(JSON_EXTRACT(fee_span, '$[4]')) as INT) as avgFee_75,
         CAST(AVG(JSON_EXTRACT(fee_span, '$[5]')) as INT) as avgFee_90,
         CAST(AVG(JSON_EXTRACT(fee_span, '$[6]')) as INT) as avgFee_100
-      FROM blocks`;
+      FROM blocks
+      WHERE stale = 0`;
 
       if (interval !== null) {
-        query += ` WHERE blockTimestamp BETWEEN DATE_SUB(NOW(), INTERVAL ${interval}) AND NOW()`;
+        query += ` AND blockTimestamp BETWEEN DATE_SUB(NOW(), INTERVAL ${interval}) AND NOW()`;
       }
 
       query += ` GROUP BY UNIX_TIMESTAMP(blockTimestamp) DIV ${div}`;
@@ -773,10 +808,11 @@ class BlocksRepository {
         CAST(AVG(height) as INT) as avgHeight,
         CAST(AVG(UNIX_TIMESTAMP(blockTimestamp)) as INT) as timestamp,
         CAST(AVG(size) as INT) as avgSize
-      FROM blocks`;
+      FROM blocks
+      WHERE stale = 0`;
 
       if (interval !== null) {
-        query += ` WHERE blockTimestamp BETWEEN DATE_SUB(NOW(), INTERVAL ${interval}) AND NOW()`;
+        query += ` AND blockTimestamp BETWEEN DATE_SUB(NOW(), INTERVAL ${interval}) AND NOW()`;
       }
 
       query += ` GROUP BY UNIX_TIMESTAMP(blockTimestamp) DIV ${div}`;
@@ -798,10 +834,11 @@ class BlocksRepository {
         CAST(AVG(height) as INT) as avgHeight,
         CAST(AVG(UNIX_TIMESTAMP(blockTimestamp)) as INT) as timestamp,
         CAST(AVG(weight) as INT) as avgWeight
-      FROM blocks`;
+      FROM blocks
+      WHERE stale = 0`;
 
       if (interval !== null) {
-        query += ` WHERE blockTimestamp BETWEEN DATE_SUB(NOW(), INTERVAL ${interval}) AND NOW()`;
+        query += ` AND blockTimestamp BETWEEN DATE_SUB(NOW(), INTERVAL ${interval}) AND NOW()`;
       }
 
       query += ` GROUP BY UNIX_TIMESTAMP(blockTimestamp) DIV ${div}`;
@@ -816,11 +853,12 @@ class BlocksRepository {
 
   /**
    * Get a list of blocks that have been indexed
+   * (includes stale blocks)
    */
-  public async $getIndexedBlocks(): Promise<{ height: number, hash: string }[]> {
+  public async $getIndexedBlocks(): Promise<{ height: number, hash: string, stale: boolean }[]> {
     try {
-      const [rows] = await DB.query(`SELECT height, hash FROM blocks ORDER BY height DESC`) as RowDataPacket[][];
-      return rows as { height: number, hash: string }[];
+      const [rows] = await DB.query(`SELECT height, hash, stale FROM blocks ORDER BY height DESC`) as RowDataPacket[][];
+      return rows as { height: number, hash: string, stale: boolean }[];
     } catch (e) {
       logger.err('Cannot generate block size and weight history. Reason: ' + (e instanceof Error ? e.message : e));
       throw e;
@@ -865,7 +903,7 @@ class BlocksRepository {
    */
   public async $getOldestConsecutiveBlock(): Promise<any> {
     try {
-      const [rows]: any = await DB.query(`SELECT height, UNIX_TIMESTAMP(blockTimestamp) as timestamp, difficulty, bits FROM blocks ORDER BY height DESC`);
+      const [rows]: any = await DB.query(`SELECT height, UNIX_TIMESTAMP(blockTimestamp) as timestamp, difficulty, bits FROM blocks WHERE stale = 0 ORDER BY height DESC`);
       for (let i = 0; i < rows.length - 1; ++i) {
         if (rows[i].height - rows[i + 1].height > 1) {
           return rows[i];
@@ -929,7 +967,7 @@ class BlocksRepository {
         SELECT height, hash
         FROM blocks
         WHERE height >= ${minHeight} AND height <= ${maxHeight} AND
-          (utxoset_size IS NULL OR total_input_amt IS NULL)
+          (utxoset_size IS NULL OR total_input_amt IS NULL) AND stale = 0
       `);
       return blocks;
     } catch (e) {
@@ -940,6 +978,7 @@ class BlocksRepository {
 
   /**
    * Get all indexed blocks with missing coinbase addresses
+   * (includes stale blocks)
    */
   public async $getBlocksWithoutCoinbaseAddresses(): Promise<any> {
     try {
@@ -1047,6 +1086,34 @@ class BlocksRepository {
       );
     } catch (e) {
       logger.err(`Cannot update block first seen time. Reason: ` + (e instanceof Error ? e.message : e));
+      throw e;
+    }
+  }
+
+  /**
+   * Change which block at a height belongs to the canonical chain
+   * 
+   * @param hash
+   * @param height
+   */
+  public async $setCanonicalBlockAtHeight(hash: string | null, height: number): Promise<void> {
+    try {
+      // do this first, so that we fail if the block hasn't actually been indexed yet
+      if (hash) {
+        await DB.query(`
+          UPDATE blocks SET stale = 0
+          WHERE hash = ?`,
+          [hash]
+        );
+      }
+      // all other blocks at this height must be stale
+      await DB.query(`
+        UPDATE blocks SET stale = 1
+        WHERE height = ? AND hash != ?`,
+        [height, hash ?? '']
+      );
+    } catch (e) {
+      logger.err(`Cannot set canonical block at height. Reason: ` + (e instanceof Error ? e.message : e));
       throw e;
     }
   }
