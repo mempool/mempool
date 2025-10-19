01c903c2f2a7bec2bee4560fa6c1899f490f6007
Mononaut <github@monospace.live>
2025-10-19 09:57:27 +0000

    limit stale block index by INDEXING_BLOCKS_AMOUNT

--- backend/src/api/chain-tips.ts
+++ backend/src/api/chain-tips.ts
@@ -1,7 +1,8 @@
+import config from '../config';
 import logger from '../logger';
 import { BlockExtended } from '../mempool.interfaces';
 import BlocksSummariesRepository from '../repositories/BlocksSummariesRepository';
-import { bitcoinCoreApi } from './bitcoin/bitcoin-api-factory';
+import bitcoinApi, { bitcoinCoreApi } from './bitcoin/bitcoin-api-factory';
 import bitcoinClient from './bitcoin/bitcoin-client';
 import { IEsploraApi } from './bitcoin/esplora-api.interface';
 import blocks from './blocks';
@@ -42,6 +43,13 @@ class ChainTips {
     try {
       this.chainTips = await bitcoinClient.getChainTips();
 
+      const activeTipHeight = this.chainTips.find(tip => tip.status === 'active')?.height || (await bitcoinApi.$getBlockHeightTip());
+      let minIndexHeight = 0;
+      const indexedBlockAmount = Math.min(config.MEMPOOL.INDEXING_BLOCKS_AMOUNT, activeTipHeight);
+      if (indexedBlockAmount > 0) {
+        minIndexHeight = Math.max(0, activeTipHeight - indexedBlockAmount + 1);
+      }
+
       const start = Date.now();
       const breakAt = start + 10000;
       let newOrphans = 0;
@@ -64,11 +72,14 @@ class ChainTips {
                   prevhash: block.previousblockhash,
                 };
                 this.blockCache[hash] = orphan;
-                if (this.indexingQueue.length < this.maxIndexingQueueSize) {
-                  this.indexingQueue.push({ block, tip: orphan });
-                } else {
-                  // re-fetch blocks lazily if the queue is big to keep memory usage sane
-                  this.indexingQueue.push({ blockhash: hash, tip: orphan });
+                // don't index stale blocks below the INDEXING_BLOCKS_AMOUNT cutoff
+                if (block.height >= minIndexHeight) {
+                  if (this.indexingQueue.length < this.maxIndexingQueueSize) {
+                    this.indexingQueue.push({ block, tip: orphan });
+                  } else {
+                    // re-fetch blocks lazily if the queue is big to keep memory usage sane
+                    this.indexingQueue.push({ blockhash: hash, tip: orphan });
+                  }
                 }
               }
             }
