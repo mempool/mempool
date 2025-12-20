2f6d672abe6d55596503050e7761e4687f56860d
Felipe Knorr Kuhn <git@knorrium.info>
2025-12-20 16:59:15 +0000

    Use the local variables instead

--- docker/frontend/entrypoint.sh
+++ docker/frontend/entrypoint.sh
@@ -6,8 +6,8 @@ __MEMPOOL_FRONTEND_HTTP_PORT__=${FRONTEND_HTTP_PORT:=8080}
 __PROXIED_SERVICES__=${PROXIED_SERVICES:=false}
 __PROXIED_SERVICES_HOST__=${PROXIED_SERVICES_HOST:=https://mempool.space}
 
-if [ "${PROXIED_SERVICES}" = "true" ]; then
-  sed -i "s|proxy_pass https://mempool.space;|proxy_pass ${PROXIED_SERVICES_HOST};|g" /etc/nginx/conf.d/nginx-mempool.conf
+if [ "${__PROXIED_SERVICES__}" = "true" ]; then
+  sed -i "s|proxy_pass https://mempool.space;|proxy_pass ${__PROXIED_SERVICES_HOST__};|g" /etc/nginx/conf.d/nginx-mempool.conf
 fi
 
 sed -i "s/__MEMPOOL_BACKEND_MAINNET_HTTP_HOST__/${__MEMPOOL_BACKEND_MAINNET_HTTP_HOST__}/g" /etc/nginx/conf.d/nginx-mempool.conf
