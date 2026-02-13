d48590176576f6794e6ef20347cd8006a2bfdc0f
algodoncin <179851883+algodondev@users.noreply.github.com>
2026-02-13 00:09:49 -0600

    Feat: fiat shortener for amounts greater than a million units

--- frontend/src/app/shared/pipes/fiat-shortener.pipe.ts
+++ frontend/src/app/shared/pipes/fiat-shortener.pipe.ts
@@ -1,4 +1,3 @@
-import { formatCurrency, getCurrencySymbol } from '@angular/common';
 import { Inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { StateService } from '@app/services/state.service';
@@ -21,28 +20,24 @@ export class FiatShortenerPipe implements PipeTransform {
   }
 
   transform(num: number, ...args: any[]): unknown {
-    const digits = args[0] || 1;
-    const currency = args[1] || this.currency || 'USD';
+    const currency = args[0] || this.currency || 'USD';
 
-    if (num < 1000) {
-      return new Intl.NumberFormat(this.locale, { style: 'currency', currency, maximumFractionDigits: 1 }).format(num);
+    if (num < 1e6) {
+      return new Intl.NumberFormat(this.locale, { style: 'currency', currency, maximumFractionDigits: 0 }).format(num);
     }
 
-    const lookup = [
-      { value: 1, symbol: '' },
-      { value: 1e3, symbol: 'k' },
-      { value: 1e6, symbol: 'M' },
-      { value: 1e9, symbol: 'B' },
-      { value: 1e12, symbol: 'T' },
-      { value: 1e15, symbol: 'P' },
-      { value: 1e18, symbol: 'E' }
-    ];
-    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
-    const item = lookup.slice().reverse().find((item) => num >= item.value);
-
-    let result = item ? (num / item.value).toFixed(digits).replace(rx, '$1') : '0';
-    result = new Intl.NumberFormat(this.locale, { style: 'currency', currency, maximumFractionDigits: 0 }).format(item ? num / item.value : 0);
-
-    return result + item.symbol;
+    const format = new Intl.NumberFormat(this.locale, { style: 'currency', currency, maximumFractionDigits: 1 });
+    const parts = format.formatToParts(num / 1e6);
+    let final = '';
+    let symbolAdded = false;
+    for (const part of parts.reverse()) {
+      if ((part.type === 'integer' || part.type === 'fraction') && !symbolAdded) {
+        final = part.value + 'M' + final;
+        symbolAdded = true;
+      } else {
+        final = part.value + final;
+      }
+    }
+    return final;
   }
 }
