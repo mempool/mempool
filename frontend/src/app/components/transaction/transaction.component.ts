a4ef5a941b9ff8f8de794f8593eed36cd7106118
Mononaut <github@monospace.live>
2026-02-26 10:11:14 +0000

    demo integration of matomo replacement

--- frontend/src/app/components/transaction/transaction.component.ts
+++ frontend/src/app/components/transaction/transaction.component.ts
@@ -39,6 +39,7 @@ import { PartnerCodeService } from '@app/services/partner-code.service';
 import { ZONE_SERVICE } from '@app/injection-tokens';
 import { MiningService, MiningStats } from '@app/services/mining.service';
 import { ETA, EtaService } from '@app/services/eta.service';
+import { AnalyticsService } from '@app/services/analytics.service';
 
 export interface Pool {
   id: number;
@@ -217,6 +218,7 @@ export class TransactionComponent implements OnInit, AfterViewInit, OnDestroy {
     private priceService: PriceService,
     private storageService: StorageService,
     private partnerCodeService: PartnerCodeService,
+    private analyticsService: AnalyticsService,
     private miningService: MiningService,
     private etaService: EtaService,
     private cd: ChangeDetectorRef,
@@ -701,6 +703,7 @@ export class TransactionComponent implements OnInit, AfterViewInit, OnDestroy {
           this.setupGraph();
 
           if (!tx.status?.confirmed) {
+            this.analyticsService.action('/tx/:txid', 'unconfirmed-tx');
             if (tx.firstSeen) {
               this.transactionTime = tx.firstSeen;
             } else {
