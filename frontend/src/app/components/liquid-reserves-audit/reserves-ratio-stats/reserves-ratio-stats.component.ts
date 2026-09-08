32b4864d21002a675a0ab4d1d7e7b6ca3b5ed14d
natsoni <natsoni@protonmail.com>
2026-09-08 15:23:07 +0200

    Update liquid reserves ratio stats

--- frontend/src/app/components/liquid-reserves-audit/reserves-ratio-stats/reserves-ratio-stats.component.ts
+++ frontend/src/app/components/liquid-reserves-audit/reserves-ratio-stats/reserves-ratio-stats.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable, combineLatest, map } from 'rxjs';
+import { CurrentPegs } from '@interfaces/node-api.interface';
 
 @Component({
   selector: 'app-reserves-ratio-stats',
@@ -10,46 +11,35 @@ import { Observable, map } from 'rxjs';
 })
 export class ReservesRatioStatsComponent implements OnInit {
   @Input() fullHistory$: Observable<any>;
+  @Input() currentReserves$: Observable<CurrentPegs>;
+  @Input() currentPeg$: Observable<CurrentPegs>;
   @Input() emergencyUtxosStats$: Observable<any>;
-  unbackedMonths$: Observable<any>;
-
-  constructor() { }
+  incidentCount$: Observable<{ total: number | null }>;
+  reserveBalance$: Observable<{ amount: number }>;
 
   ngOnInit(): void {
-    if (!this.fullHistory$) {
-      return;
-    }
-    this.unbackedMonths$ = this.fullHistory$
-      .pipe(
+    if (this.fullHistory$) {
+      this.incidentCount$ = this.fullHistory$.pipe(
         map((fullHistory) => {
           const pegsSeries = fullHistory?.liquidPegs?.series || [];
           const reservesSeries = fullHistory?.liquidReserves?.series || [];
           if (pegsSeries.length < 2 || pegsSeries.length !== reservesSeries.length) {
-            return {
-              historyComplete: false,
-              total: null
-            };
+            return { total: null };
           }
           // Only check the last 3 years
           let ratioSeries = reservesSeries.map((value: number, index: number) => value / pegsSeries[index]);
-          ratioSeries = ratioSeries.slice(Math.max(ratioSeries.length - 36, 0));
-          let total = 0;
-          for (let i = 0; i < ratioSeries.length; i++) {
-            if (ratioSeries[i] < 0.95) {
-              total++;
-            }
-          }
-          const currentBalance = reservesSeries[reservesSeries.length - 1];
-          const previousBalance = reservesSeries[reservesSeries.length - 2];
-          const delta1m = currentBalance - previousBalance;
-          return {
-            historyComplete: true,
-            total: total,
-            delta1m: delta1m,
-          };
+          return { total: ratioSeries.slice(-36).filter((ratio: number) => ratio < 0.95).length };
         })
       );
+    }
 
+    if (!this.currentReserves$ || !this.currentPeg$) {
+      return;
+    }
+    this.reserveBalance$ = combineLatest([this.currentReserves$, this.currentPeg$]).pipe(
+      map(([reserves, pegs]) => ({
+        amount: (+reserves.amount - +pegs.amount) / 100000000,
+      }))
+    );
   }
-
 }
